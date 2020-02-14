54846936856dcf54466a30061f3a77879ea5713f
Water.Li <11011968@qq.com>
2020-02-14 12:56:26 +0800

    change

--- src/shell/controls/switch.js
+++ src/shell/controls/switch.js
@@ -6,25 +6,37 @@ export class OpSwitch extends RXComponent{
     this.fieldName = fieldName
     this.cssClass('op-switch')
     this.value = value
-    this.onValue = 'on'
-    this.offValue = 'off'
+    this.onValue = ''
+    this.offValue = ''
     this.valueChanged=(value, fdName)=>{}
     this.domOn('onclick',()=>{
       this.changeValue()
+      this.updateState()
     })
+  }
 
+  render(parentDomElement){
+    this.updateState()
+    return super.render(parentDomElement)
   }
 
   changeValue(){
     if(this.value === this.onValue){
       this.value = this.offValue
-      this.removeCssClass('on')
     }
     else{
       this.value = this.onValue
-      this.cssClass('on')
     }
     this.valueChanged(this.value, this.fieldName)
+  }
+
+  updateState(){
+    if(this.value === this.offValue){
+      this.removeCssClass('on')
+    }
+    else{
+      this.cssClass('on')
+    }
   }
 
 }
