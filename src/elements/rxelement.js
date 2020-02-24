99a3d408ce89202914af13cbd7b4580cbc62e225
Water.Li <11011968@qq.com>
2020-02-24 14:05:42 +0800

    change

--- src/elements/rxelement.js
+++ src/elements/rxelement.js
@@ -4,6 +4,40 @@ import parkMiniEditbar from "../core/park-mini-editbar"
 import {addonClasses} from "./schemas/general/classes"
 import {addonAttributes} from "./schemas/general/attributes"
 import {addonGeneralTextfield} from "./schemas/general/textfield"
+import {addonHTMLId} from "./schemas/general/id"
+import {addonUtilColor} from "./schemas/utilities/color"
+import {addonUtilBorder} from "./schemas/utilities/border"
+import {addonUtilPadding} from "./schemas/utilities/padding"
+import {addonUtilMargin} from "./schemas/utilities/margin"
+import {addonUtilClearfix} from "./schemas/utilities/clearfix"
+import {addonUtilDisplay} from "./schemas/utilities/display"
+import {addonUtilEmbed} from "./schemas/utilities/embed"
+import {addonUtilResponsiveItem} from "./schemas/utilities/embed/responsive-item"
+import {addonUtilFlex} from "./schemas/utilities/flex"
+import {addonUtilFloat} from "./schemas/utilities/float"
+import {addonUtilTextHide} from "./schemas/utilities/text-hide"
+import {addonUtilOverflow} from "./schemas/utilities/overflow"
+import {addonUtilPosition} from "./schemas/utilities/position"
+import {addonUtilScreenReaders} from "./schemas/utilities/screen-readers"
+import {addonUtilShadow} from "./schemas/utilities/shadow"
+import {addonUtilSizing} from "./schemas/utilities/sizing"
+import {addonUtilStretchedLink} from "./schemas/utilities/stretched-link"
+import {addonUtilText} from "./schemas/utilities/text"
+import {addonUtilVerticalAlignment} from "./schemas/utilities/vertical-align"
+import {addonUtilVisibility} from "./schemas/utilities/visibility"
+
+import {addonHeadingPseudo} from "./schemas/heading/pseudo-heading"
+import {addonHeadingDisplay} from "./schemas/heading/display"
+import {addonGridRow} from "./schemas/content/row"
+import {addonWidth} from "./schemas/column/col-width"
+import {addonOffset} from "./schemas/column/col-offset"
+import {addonAlignSelf} from "./schemas/column/col-align-self"
+import {addonOrder} from "./schemas/column/col-order"
+import {addonUtilMarginAuto} from "./schemas/column/margin-auto"
+import {addonTypyLead} from "./schemas/content/lead"
+import {addonTypyInitialism} from "./schemas/content/Initialism"
+import {addonTypyBlockquote} from "./schemas/content/blockquote"
+import {addonTypyBlockquoteFooter} from "./schemas/content/blockquote-footer"
 
 export class RXElement extends Node{
   constructor() {
@@ -54,6 +88,46 @@ export class RXElement extends Node{
     //addonGeneralTextfield(this)
     addonClasses(this)
     addonAttributes(this)
+    addonHTMLId(this)
+    addonHeadingPseudo(this, 'typographyOptions')
+    addonHeadingDisplay(this, 'typographyOptions')
+    addonGridRow(this, 'typographyOptions')
+    let col = addonWidth(this, 'typographyOptions')
+    col.schema.xs.label = 'Column'
+    col.schema.sm.label = 'Column'
+    col.schema.md.label = 'Column'
+    col.schema.lg.label = 'Column'
+    col.schema.xl.label = 'Column'
+    addonOffset(this, 'typographyOptions')
+    addonAlignSelf(this, 'typographyOptions')
+    addonOrder(this, 'typographyOptions')
+    addonUtilMarginAuto(this, 'typographyOptions')
+    addonTypyLead(this)
+    addonTypyInitialism(this)
+    addonTypyBlockquote(this)
+    addonTypyBlockquoteFooter(this)
+
+    addonUtilColor(this)
+    addonUtilBorder(this)
+    addonUtilMargin(this)
+    addonUtilPadding(this)
+    addonUtilClearfix(this)
+    addonUtilDisplay(this)
+    addonUtilEmbed(this)
+    addonUtilResponsiveItem(this) //need to move to other tags
+    addonUtilFlex(this)
+    addonUtilFloat(this)
+    addonUtilTextHide(this)
+    addonUtilOverflow(this)
+    addonUtilPosition(this)
+    addonUtilScreenReaders(this)
+    addonUtilShadow(this)
+    addonUtilSizing(this)
+    addonUtilStretchedLink(this) //need to move to a tag
+    addonUtilText(this)
+    addonUtilVerticalAlignment(this)
+    addonUtilVisibility(this)
+
   }
 
   clone(){
