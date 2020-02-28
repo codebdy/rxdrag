df1fbd657fddb5e569da6fba5c8cb6592c4589a4
Water.Li <11011968@qq.com>
2020-02-28 21:11:51 +0800

    change

--- src/shell/themes.js
+++ src/shell/themes.js
@@ -2,7 +2,7 @@
 let themes = {
   base:{
     cssFiles:[
-      'https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css',
+      'style/bootstrap-4.4.1-dist/css/bootstrap.min.css',
       'style/rxeditor.css',
       'style/font-awesome-4.7.0/css/font-awesome.min.css',
     ],
@@ -15,10 +15,7 @@ let themes = {
       'themes/agency/vendor/bootstrap/css/bootstrap.min.css',
       'themes/agency/vendor/fontawesome-free/css/all.min.css',
       'style/font-awesome-4.7.0/css/font-awesome.min.css',
-      'https://fonts.googleapis.com/css?family=Montserrat:400,700',
-      'https://fonts.googleapis.com/css?family=Kaushan+Script',
-      'https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic',
-      'https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700',
+      'themes/agency/vendor/google-fonts/fonts.css',
       'themes/agency/css/agency.min.css',
     ],
     jsFiles:[
@@ -27,7 +24,7 @@ let themes = {
       'themes/agency/vendor/jquery-easing/jquery.easing.min.js',
       'themes/agency/js/jqBootstrapValidation.js',
       'themes/agency/js/contact_me.js',
-      //'themes/agency/js/agency.min.js',
+      'themes/agency/js/agency.min.js',
     ],
 
     uiBlocks:[
