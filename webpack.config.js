966166218bf297a4ec6c15a258a9fceaf7a53c05
Water.Li <11011968@qq.com>
2020-03-10 11:10:42 +0800

    add boostrap and jquery package

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,14 @@ const path = require('path');
 
 module.exports = {
   // JavaScript 执行入口文件
-  entry: './src/index.js',
-  //mode: 'development',
+  entry: {
+  	index : './src/index.js',
+  	core : './src/core.js'
+  },
+  mode: 'development',
   output: {
     // 把所有依赖的模块合并输出到一个 bundle.js 文件
-    filename: 'index.js',
+    filename: '[name].js',
     // 输出文件都放到 dist 目录下
     path: path.resolve(__dirname, './dist'),
   }
