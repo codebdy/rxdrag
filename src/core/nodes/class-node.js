11740d9b91b9c64a221e4b7978f7d6094af9489e
Water.Li <11011968@qq.com>
2020-03-22 17:07:16 +0800

    continue

--- src/core/nodes/class-node.js
+++ src/core/nodes/class-node.js
@@ -4,14 +4,12 @@ import {add, remove} from "../../basic/rxarray"
 export class ClassNode extends RXNode{
   constructor(cssClass) {
     super()
-    	//add(cssClass, this.meta.classList)
-    	this.cssClass = cssClass
-    	this.label = cssClass.replace(cssClass[0],cssClass[0].toUpperCase())
-	}
+      //add(cssClass, this.meta.classList)
+      this.cssClass = cssClass
+      this.label = cssClass.replace(cssClass[0],cssClass[0].toUpperCase())
+  }
 
   make(){
     return new ClassNode(this.cssClass)
   }
-
-
 }
