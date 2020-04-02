2f97fd054af46b0ec95c5aaa3392de3dcb354ae4
Water.Li <11011968@qq.com>
2020-04-02 15:07:08 +0800

    Update canvas.js

--- src/core/canvas.js
+++ src/core/canvas.js
@@ -11,10 +11,6 @@ export class Canvas extends Node{
     this.stateChanged = (oldState, newState)=>{}
 
     this.rule = {dropInMargin : 0}
-    this.rule.editMarginStyle = {paddingX:10, paddingY:10}
-    //this.rule.heightDropMargin = 0
-    //this.rule.widthDropMargin = 0
-    //this.rule.dropMargin = 0;
     this.rule.acceptedChildren=''
     this.rule.rejectChildren = ['col','thead', 'tbody', 
                            'th', 'tr', 'td']
@@ -38,10 +34,9 @@ export class Canvas extends Node{
     model.styles['min-height'] = "calc(100vh)"
     model.styles.cursor = 'default'
     model.styles['border'] = '0'
-    //model.styles['padding-bottom'] = '40px'
-    //model.styles['background'] = '#8cdb27'
-    //model.styles['padding-top'] = '25px'
-    //model.styles['padding-right'] = '5px'
+    if(this.state !== this.previewState){
+      model.styles['padding'] = '10px'
+    }
     model.classList.push('canvas')
     model.toolbar = ''
     model.label = ''
