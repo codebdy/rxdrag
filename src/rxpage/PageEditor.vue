f792b9dac11e6fe88698a75724ab09edb4224992
Water.Li <11011968@qq.com>
2020-04-06 23:11:03 +0800

    Update PageEditor.vue

--- src/rxpage/PageEditor.vue
+++ src/rxpage/PageEditor.vue
@@ -1,28 +1,28 @@
 <template>
   <div class="rxpage-editor">
-    <div class="toolbar">
+    <div class="rx-toolbar">
       <div class="left">
-        <div class="icon-button big" 
+        <div class="rx-icon-button big" 
           title = "XL"
         >
           <i class="fas fa-tv"></i>
         </div>
-        <div class="icon-button"
+        <div class="rx-icon-button"
           title = "LG"
         >
           <i class="fas fa-desktop"></i>
         </div>
-        <div class="icon-button"
+        <div class="rx-icon-button"
           title = "MD"
         >
           <i class="fas fa-laptop"></i>
         </div>
-        <div class="icon-button"
+        <div class="rx-icon-button"
           title = "SM"
         >
           <i class="fas fa-tablet-alt"></i>
         </div>
-        <div class="icon-button"
+        <div class="rx-icon-button"
            title = "XS"
         >
           <i class="fas fa-mobile-alt"></i>
@@ -30,48 +30,51 @@
 
       </div>
       <div class="right ">
-        <div class="icon-button" title = "">
+        <div class="rx-icon-button" title = "">
           <i class="fas fa-tools"></i>
         </div>
-        <div class="icon-button" title = "">
+        <div class="rx-icon-button" title = "">
           <i class="fas fa-paint-brush"></i>
         </div>
-        <div class="icon-button small" title = "">
+        <div class="rx-icon-button small" title = "">
           <i class="fas fa-project-diagram"></i>
         </div>
-        <div class="icon-button" title = "">
+        <div class="rx-icon-button" title = "">
           <i class="far fa-square"></i>
         </div>
-        <div class="icon-button" title = "">
+        <div class="rx-icon-button" title = "">
           <i class="fas fa-arrows-alt-h"></i>
         </div>
-        <div class="icon-button" title = "">
+        <div class="rx-icon-button" title = "">
           <i class="fas fa-arrows-alt-v"></i>
         </div>
-        <div class="icon-button" title = "">
+        <div class="rx-icon-button" title = "">
           <i class="fas fa-eye"></i>
         </div>
-        <div class="icon-button" title = "">
+        <div class="rx-icon-button" title = "">
           <i class="fas fa-code"></i>
         </div>
-        <div class="icon-button small" title = "">
+        <div class="rx-icon-button small" title = "">
           <i class="fas fa-undo"></i>
         </div>
-        <div class="icon-button small" title = "">
+        <div class="rx-icon-button small" title = "">
           <i class="fas fa-redo"></i>
         </div>
-        <div class="icon-button ex-big" title = "">
+        <div class="rx-icon-button ex-big" title = "">
           <i class="fas fa-arrows-alt" style="transform:rotate(45deg);font-size:18px;"></i>
         </div>
-        <div class="icon-button ex-big" title = "">
+        <div class="rx-icon-button ex-big" title = "">
           <i class="fas fa-question-circle" ></i>
         </div>
-        <div class="icon-button big" title = "">
+        <div class="rx-icon-button big" title = "">
           <i class="fas fa-layer-group" ></i>
         </div>
       </div>
     </div>
     <div class="canvas"></div>
+    <div class="rx-toolbox">
+      <div>工具箱</div>
+    </div>
   </div>
 </template>
 
@@ -107,7 +110,7 @@ export default {
     flex-flow: column;
   }
 
-  .toolbar{
+  .rxpage-editor .rx-toolbar{
     height: 40px;
     display: flex;
     flex-flow: row;
@@ -120,17 +123,13 @@ export default {
     font-size: 13px;
   }
 
-  .toolbar .left{
+  .rxpage-editor .rx-toolbar .left{
     display: flex;
     flex-flow: row;
   }
 
-  .logo{
-    font-size:18px;
-    font-weight: bold;
-  }
 
-  .toolbar .icon-button{
+  .rxpage-editor .rx-toolbar .rx-icon-button{
     width: 30px;
     height: 30px;
     display: flex;
@@ -140,41 +139,41 @@ export default {
     border-radius: 2px;
   }
 
-  .toolbar .right{
+  .rxpage-editor .rx-toolbar .right{
     display: flex;
     flex-flow: row;
   }
-  .toolbar .right .icon-button{
+  .rxpage-editor .rx-toolbar .right .rx-icon-button{
     font-size: 14px;
   }
 
-  .toolbar .icon-button:hover{
+  .rxpage-editor .rx-toolbar .rx-icon-button:hover{
     background: rgba(0,0,0,0.3);
   }
 
-  .toolbar .icon-button.active{
+  .rxpage-editor .rx-toolbar .rx-icon-button.active{
     background: rgba(0,0,0,0.5);
   }
 
-  .toolbar .icon-button.big i{
+  .rxpage-editor .rx-toolbar .rx-icon-button.big i{
     font-size: 14px;
   }
 
-  .toolbar .icon-button.small i{
+  .rxpage-editor .rx-toolbar .rx-icon-button.small i{
     font-size: 12px;
   }
 
-  .toolbar .icon-button.ex-big i{
+  .rxpage-editor .rx-toolbar .rx-icon-button.ex-big i{
     font-size: 16px;
   }
 
 
-  .toolbar .icon-button.disabled{
+  .rxpage-editor .rx-toolbar .rx-icon-button.disabled{
     pointer-events: none;
     position: relative;
   }
 
-  .toolbar .icon-button.disabled::after{
+  .rxpage-editor .rx-toolbar .rx-icon-button.disabled::after{
     content: "";
     position: absolute;
     top:0;
@@ -183,6 +182,16 @@ export default {
     height: 100%;
     background: rgba(73,76,69,0.7);
     z-index: 1;
+  }
+
+  .rx-toolbox{
+    position: fixed;
+    top:60px;
+    right: 10px;
+    width: 220px;
+    height: calc(100% - 120px);
+    background: #fff;
+    box-shadow: 1px 1px 5px #888;
   }
 
 </style>
