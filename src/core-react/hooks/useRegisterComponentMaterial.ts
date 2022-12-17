610a9dd8ef89bdcf1c216e7e56f132d37965b03f
Water.Li <lizongbin@hotmail.com>
2022-12-17 20:49:46 +0800

    Update useRegisterComponentMaterial.ts

--- src/core-react/hooks/useRegisterComponentMaterial.ts
+++ src/core-react/hooks/useRegisterComponentMaterial.ts
@@ -1,6 +1,7 @@
 import { IComponentMaterial } from "core-react/interfaces";
+import { IResourceNode } from "core/interfaces";
 import { isStr } from "core/utils/types";
-import { useEffect, useMemo } from "react";
+import { useEffect, useState } from "react";
 import { useComponentManager } from "./useComponentManager";
 import { useDesignComponents } from "./useDesignComponents";
 import { useLocalesManager } from "./useLocalesManager";
@@ -8,12 +9,18 @@ import { usePreviewComponents } from "./usePreviewComponents";
 import { useResourceManager } from "./useResourceManager";
 
 export function useRegisterComponentMaterial(meterial: IComponentMaterial) {
+  const[resourceNode, setResourceNode] = useState<IResourceNode>()
   const resourceManager = useResourceManager()
   const componentManager = useComponentManager()
   const localesManager = useLocalesManager()
   const { registerComponents: registerDesignComponents } = useDesignComponents()
   const { registerComponents: registerPreviewComponents } = usePreviewComponents()
-  const resoureNode = useMemo(() => {
+
+
+  //注册设计渲染跟预览渲染用的组件
+  useEffect(() => {
+    const designers = { [meterial.componentName]: meterial.designer }
+    const previews = { [meterial.componentName]: meterial.component }
     componentManager?.registerComponents(meterial)
     if (meterial.designerLocales) {
       localesManager?.registerComponentLocales(meterial.componentName, meterial.designerLocales)
@@ -27,6 +34,8 @@ export function useRegisterComponentMaterial(meterial: IComponentMaterial) {
       if (slotMaterial === true || slotMaterial === undefined || isStr(slotMaterial)) {
         continue
       }
+      designers[slotMaterial.componentName] = slotMaterial.designer
+      previews[slotMaterial.componentName] = slotMaterial.component
 
       componentManager?.registerComponents(slotMaterial)
       if (slotMaterial.designerLocales) {
@@ -40,33 +49,14 @@ export function useRegisterComponentMaterial(meterial: IComponentMaterial) {
       }
     }
 
-    //这行在最后
-    if (meterial.resource) {
-      const resources = resourceManager?.registerResources(meterial.resource)
-      return resources?.[0]
-    }
-    return undefined
-  }, [componentManager, localesManager, meterial, resourceManager])
-
-  const [designers, previews] = useMemo(() => {
-    const designs = { [meterial.componentName]: meterial.designer }
-    const prevws = { [meterial.componentName]: meterial.component }
-    for (const key of Object.keys(meterial.slots || {})) {
-      const slotMaterial = meterial.slots?.[key]
-      if (slotMaterial === true || slotMaterial === undefined || isStr(slotMaterial)) {
-        continue
-      }
-      designs[slotMaterial.componentName] = slotMaterial.designer
-      prevws[slotMaterial.componentName] = slotMaterial.component
-    }
-    return [designs, prevws]
-  }, [meterial.component, meterial.componentName, meterial.designer, meterial.slots])
-
-
-  useEffect(() => {
     registerDesignComponents(designers)
     registerPreviewComponents(previews)
-  }, [designers, previews, registerDesignComponents, registerPreviewComponents])
 
-  return resoureNode
+    if (meterial.resource) {
+      const resources = resourceManager?.registerResources(meterial.resource)
+      setResourceNode(resources?.[0])
+    }
+  }, [componentManager, localesManager, meterial, registerDesignComponents, registerPreviewComponents, resourceManager])
+
+  return resourceNode
 }
