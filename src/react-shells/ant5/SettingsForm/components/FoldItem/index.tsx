4606562bc0a2dd3f527ec974e3527c88f1bf7b7b
Water.Li <lizongbin@hotmail.com>
2022-12-20 11:54:02 +0800

    folditem can expand

--- src/react-shells/ant5/SettingsForm/components/FoldItem/index.tsx
+++ src/react-shells/ant5/SettingsForm/components/FoldItem/index.tsx
@@ -1,18 +1,41 @@
 import { RightOutlined } from "@ant-design/icons"
 import { Form } from "antd"
-import React, { memo } from "react"
+import React, { memo, useCallback, useContext, useMemo, useState } from "react"
 import { CSSProperties } from "styled-components"
+import { FoldItemContext } from "./context"
 import "./style.less"
+import cls from "classnames"
+import { isBool } from "core/utils/types"
 
 export type FoldItemProps = {
   children?: React.ReactNode
 }
 
 export const FoldItem = memo((props: FoldItemProps) => {
+  const [expand, setExpand] = useState<boolean>()
+
+  const handleExpandChange = useCallback((exp?: boolean | ((previousState?: boolean) => boolean)) => {
+    
+    if (isBool(exp)) {
+      setExpand(exp)
+    } else {
+      setExpand(exp?.(expand))
+    }
+  }, [expand])
+
+  const param = useMemo(() => {
+    return {
+      expand,
+      setExpand: handleExpandChange
+    }
+  }, [expand, handleExpandChange])
+
   return (
-    <div className='rx-fold-item'>
-      {props.children}
-    </div>
+    <FoldItemContext.Provider value={param}>
+      <div className='rx-fold-item'>
+        {props.children}
+      </div>
+    </FoldItemContext.Provider>
   )
 })
 
@@ -22,11 +45,17 @@ export type FoldBaseItemProps = {
 }
 export const FoldBaseItem = memo((props: FoldBaseItemProps) => {
   const { label, children, } = props
+  const { expand, setExpand } = useContext(FoldItemContext)
+
+  const handleClick = useCallback(() => {
+    setExpand(expand => !expand)
+  }, [setExpand])
+
   return (
     <Form.Item
       label={
-        <div className="rx-fold-base-item-label">
-          <RightOutlined className="base-icon" />{label}
+        <div className="rx-fold-base-item-label" onClick={handleClick}>
+          <RightOutlined className={cls("base-icon", { expand: expand })} />{label}
         </div>
       }
     >
