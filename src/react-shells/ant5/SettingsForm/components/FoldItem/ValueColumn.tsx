15d56c2178fb7892062ace054a2b163dd2eedc61
Water.Li <lizongbin@hotmail.com>
2022-12-20 14:03:47 +0800

    backup

--- src/react-shells/ant5/SettingsForm/components/FoldItem/ValueColumn.tsx
+++ src/react-shells/ant5/SettingsForm/components/FoldItem/ValueColumn.tsx
@@ -1,15 +1,26 @@
-import { Col } from "antd"
+import { Col, Tooltip } from "antd"
 import React, { CSSProperties, memo } from "react"
 import cls from "classnames"
 
+export const ValueIcon = memo((props: { children?: React.ReactNode }) => {
+  return (
+    <div style={{ marginRight: 8, display: "flex", alignItems: "center", height: "100%" }}>
+      {props.children}
+    </div>
+  )
+})
+
 export const ValueColumn = memo((props: {
+  title?: string,
+  icon?: React.ReactNode,
   span?: number,
   onFirstLine?: boolean,
   className?: string,
   style?: CSSProperties,
   children?: React.ReactNode
 }) => {
-  const { span = 12, onFirstLine = true, className, children, style, ...other } = props
+  const { title, icon, span = 12, onFirstLine = true, className, children, style, ...other } = props
+
   return (
     <Col
       span={span}
@@ -17,6 +28,14 @@ export const ValueColumn = memo((props: {
       style={{ marginTop: !onFirstLine ? 8 : undefined, ...style }}
       {...other}
     >
+      {
+        <Tooltip title={title}>
+          <ValueIcon>
+            {icon}
+          </ValueIcon>
+        </Tooltip>
+      }
+
       {children}
     </Col>
   )
