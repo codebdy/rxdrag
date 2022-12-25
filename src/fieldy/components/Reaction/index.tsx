b16172fbd76a82154e055755f1d84096c5cef46f
Water.Li <lizongbin@hotmail.com>
2022-12-25 17:39:41 +0800

    Update index.tsx

--- src/fieldy/components/Reaction/index.tsx
+++ src/fieldy/components/Reaction/index.tsx
@@ -3,7 +3,10 @@ import { useField } from "fieldy/hooks/useField"
 import { memo, useCallback, useEffect } from "react"
 
 const PREFIX_SIBLINGS = "$siblings"
-//const PREFIX_SELF = "$self"
+const PREFIX_SELF = "$self"
+
+const $siblings = {}
+const $self = {}
 
 export const Reaction = memo((
   props: {
@@ -13,22 +16,26 @@ export const Reaction = memo((
   const fieldy = useFieldy()
   const { fieldMeta, basePath } = useField()
   const formName = useFormName()
+  const reactionParams = fieldMeta?.effects?.onFieldsValueChange
   const getFieldPaths = useCallback((fields: string[]) => {
     return fields.map(field => field.replace(PREFIX_SIBLINGS + ".", basePath ? (basePath + ".") : ""))
   }, [basePath])
 
   const handleFieldsValueChange = useCallback((newValues: any[], prevousValues: any[]) => {
-
-  }, [])
+    if(reactionParams?.jsCode){
+      // eslint-disable-next-line no-new-func
+      const func = new Function (PREFIX_SIBLINGS, PREFIX_SELF, reactionParams.jsCode)
+      func($siblings, $self)
+    }
+  }, [reactionParams?.jsCode])
 
   useEffect(() => {
-    const fieldValueChange = fieldMeta?.effects?.onFieldsValueChange
-    if (fieldValueChange?.fields && formName) {
-      const unsub = fieldy?.subscribeToFieldsValueChange(formName, getFieldPaths(fieldValueChange.fields), handleFieldsValueChange)
+    if (reactionParams?.fields && formName) {
+      const unsub = fieldy?.subscribeToFieldsValueChange(formName, getFieldPaths(reactionParams.fields), handleFieldsValueChange)
       return unsub
     }
 
-  }, [fieldMeta?.effects?.onFieldsValueChange, fieldy, formName, getFieldPaths, handleFieldsValueChange])
+  }, [fieldy, formName, getFieldPaths, handleFieldsValueChange, reactionParams?.fields])
 
   return (
     <>
