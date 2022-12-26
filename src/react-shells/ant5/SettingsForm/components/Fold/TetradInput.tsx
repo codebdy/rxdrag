8c22537bae57824bcd98b30101a506d6988e15cf
Water.Li <lizongbin@hotmail.com>
2022-12-26 21:44:43 +0800

    backup

---
+++ src/react-shells/ant5/SettingsForm/components/Fold/TetradInput.tsx
@@ -0,0 +1,111 @@
+import { memo, useCallback, useMemo } from "react"
+import { Fold, FoldBase, FoldExtra } from "../Fold"
+import { SizeInput, SizeInputItem } from "../SizeInput"
+
+
+export const TetradInput = memo((props: {
+  title?: string,
+  title1?: string,
+  title2?: string,
+  title3?: string,
+  title4?: string,
+  icon1?: string,
+  icon2?: string,
+  icon3?: string,
+  icon4?: string,
+  value?: any,
+  onChange?: (value?: any) => void,
+  keys: string[]
+}) => {
+  const { title,
+    title1,
+    title2,
+    title3,
+    title4,
+    icon1,
+    icon2,
+    icon3,
+    icon4,
+    value,
+    onChange,
+    keys
+  } = props;
+
+  const handleChange1 = useCallback((value?: string | null) => {
+    onChange?.({ [keys[0]]: value || undefined })
+  }, [keys, onChange])
+
+  const handleChange2 = useCallback((value?: string | null) => {
+    onChange?.({ [keys[1]]: value || undefined })
+  }, [keys, onChange])
+
+  const handleChange4 = useCallback((value?: string | null) => {
+    onChange?.({ [keys[2]]: value || undefined })
+  }, [keys, onChange])
+
+  const handleChange3 = useCallback((value?: string | null) => {
+    onChange?.({ [keys[3]]: value || undefined })
+  }, [keys, onChange])
+
+  const baseValue = useMemo(() => {
+    if (value?.[keys[0]] === value?.[keys[1]]
+      && value?.[keys[1]] === value?.[keys[2]] &&
+      value?.[keys[2]] === value?.[keys[3]]) {
+      return value?.[keys[0]]
+    }
+    return ""
+  }, [keys, value])
+  
+  const handleBaseChange = useCallback((value?: string | null) => {
+    onChange?.({
+      [keys[0]]: value || undefined,
+      [keys[1]]: value || undefined,
+      [keys[2]]: value || undefined,
+      [keys[3]]: value || undefined,
+    })
+  }, [keys, onChange])
+
+  return (
+    <Fold>
+      <FoldBase title={title}>
+        <BaseInput value={baseValue} onChange={handleBaseChange} />
+      </FoldBase>
+      <FoldExtra>
+        <SizeInputItem
+          title={title1}
+          icon={icon1}
+          value={value?.marginTop}
+          onChange={handleChange1}
+        />
+        <SizeInputItem
+          title={title2}
+          icon={icon2}
+          value={value?.marginRight}
+          onChange={handleChange2}
+        />
+        <SizeInputItem
+          title={title3}
+          icon={icon3}
+          value={value?.marginLeft}
+          onChange={handleChange3}
+        />
+        <SizeInputItem
+          title={title4}
+          icon={icon4}
+          value={value?.marginBottom}
+          onChange={handleChange4}
+        />
+      </FoldExtra>
+    </Fold>
+  )
+})
+
+export const BaseInput = memo((props: {
+  value?: string,
+  onChange?: (value?: string | null) => void
+}) => {
+  return (
+    <SizeInput exclude={["inherit", "auto"]} {...props} />
+  )
+})
+
--- src/react-shells/ant5/SettingsForm/components/margin/index.tsx
+++ src/react-shells/ant5/SettingsForm/components/margin/index.tsx
@@ -1,6 +1,5 @@
-import { memo, useCallback, useMemo } from "react"
-import { Fold, FoldBase, FoldExtra } from "../Fold"
-import { SizeInput, SizeInputItem } from "../SizeInput"
+import { memo } from "react"
+import { TetradInput } from "../Fold/TetradInput"
 
 export const marginTopIcon = `<svg viewBox="0 0 1024 1024" height="16" width="16" fill="currentColor" focusable="false" aria-hidden="true"><path d="M539.414911,228.284271 L765.689081,454.558441 C781.310053,470.179413 781.310053,495.506012 765.689081,511.126984 C750.06811,526.747955 724.74151,526.747955 709.120539,511.126984 L551.13,353.137 L551.13064,904 C551.13064,926.09139 533.22203,944 511.13064,944 C489.03925,944 471.13064,926.09139 471.13064,904 L471.13,353.137 L313.140741,511.126984 C297.51977,526.747955 272.19317,526.747955 256.572199,511.126984 C240.951227,495.506012 240.951227,470.179413 256.572199,454.558441 L482.846369,228.284271 C498.46734,212.6633 523.79394,212.6633 539.414911,228.284271 Z M872,80 C894.09139,80 912,97.90861 912,120 C912,142.09139 894.09139,160 872,160 L152,160 C129.90861,160 112,142.09139 112,120 C112,97.90861 129.90861,80 152,80 L872,80 Z"></path></svg>`
 
@@ -28,125 +27,27 @@ export const MarginStyleSetter = memo((props: {
   value?: IMargin,
   onChange?: (value?: IMargin) => void
 }) => {
-  const { title, topTitle, rightTitle, leftTitle, bottomTitle, value, onChange } = props;
+  const { title, topTitle, rightTitle, leftTitle, bottomTitle, ...other } = props;
 
-  const handleTopChange = useCallback((value?: string | null) => {
-    onChange?.({ marginTop: value || undefined })
-  }, [onChange])
-
-  const handleRightChange = useCallback((value?: string | null) => {
-    onChange?.({ marginRight: value || undefined })
-  }, [onChange])
-
-  const handleBottomChange = useCallback((value?: string | null) => {
-    onChange?.({ marginBottom: value || undefined })
-  }, [onChange])
-
-  const handleLeftChange = useCallback((value?: string | null) => {
-    onChange?.({ marginLeft: value || undefined })
-  }, [onChange])
-
-  const baseValue = useMemo(() => {
-    if (value?.marginBottom === value?.marginLeft
-      && value?.marginLeft === value?.marginRight &&
-      value?.marginRight === value?.marginTop) {
-      return value?.marginBottom
-    }
-    return ""
-  }, [value?.marginBottom, value?.marginLeft, value?.marginRight, value?.marginTop])
-  const handleBaseChange = useCallback((value?: string | null) => {
-    onChange?.({
-      marginTop: value || undefined,
-      marginBottom: value || undefined,
-      marginRight: value || undefined,
-      marginLeft: value || undefined,
-    })
-  }, [onChange])
 
   return (
-    <Fold>
-      <FoldBase title={title}>
-        <MarginInput value={baseValue} onChange={handleBaseChange} />
-      </FoldBase>
-      <FoldExtra>
-        <MarginTopInput
-          title={topTitle}
-          value={value?.marginTop}
-          onChange={handleTopChange}
-        />
-        <MarginRightInput
-          title={rightTitle}
-          value={value?.marginRight}
-          onChange={handleRightChange}
-        />
-        <MarginLeftInput
-          title={leftTitle}
-          value={value?.marginLeft}
-          onChange={handleLeftChange}
-        />
-        <MarginBottomInput
-          title={bottomTitle}
-          value={value?.marginBottom}
-          onChange={handleBottomChange}
-        />
-      </FoldExtra>
-    </Fold>
-  )
-})
-
-export const MarginInput = memo((props: {
-  value?: string,
-  onChange?: (value?: string | null) => void
-}) => {
-  return (
-    <SizeInput exclude={["inherit", "auto"]} {...props} />
-  )
-})
-
-export const MarginTopInput = memo((props: {
-  title?: string,
-  span?: number,
-  value?: string,
-  onChange?: (value?: string | null) => void
-}) => {
-
-  return (
-    <SizeInputItem icon={marginTopIcon} {...props} />
-  )
-})
-
-export const MarginRightInput = memo((props: {
-  title?: string,
-  span?: number,
-  value?: string,
-  onChange?: (value?: string | null) => void
-}) => {
-
-  return (
-    <SizeInputItem icon={marginRightIcon} {...props} />
-  )
-})
-
-export const MarginBottomInput = memo((props: {
-  title?: string,
-  span?: number,
-  value?: string,
-  onChange?: (value?: string | null) => void
-}) => {
-
-  return (
-    <SizeInputItem icon={marginBottomIcon} marginTop={8} {...props} />
-  )
-})
-
-export const MarginLeftInput = memo((props: {
-  title?: string,
-  span?: number,
-  value?: string,
-  onChange?: (value?: string | null) => void
-}) => {
-
-  return (
-    <SizeInputItem icon={marginLeftIcon} marginTop={8} {...props} />
+    <TetradInput
+      keys={[
+        'marginTop',
+        'marginRight',
+        'marginBottom',
+        'marginLeft',
+      ]}
+      title={title}
+      title1={topTitle}
+      title2={rightTitle}
+      title3={leftTitle}
+      title4={bottomTitle}
+      icon1={marginTopIcon}
+      icon2={marginRightIcon}
+      icon3={marginLeftIcon}
+      icon4={marginBottomIcon}
+      {...other}
+    />
   )
 })
