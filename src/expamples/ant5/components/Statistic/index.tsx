78cfd406069cec2680d9e74a13ab42a1426b7154
Water.Li <lizongbin@hotmail.com>
2022-12-29 20:41:31 +0800

    backup

---
+++ src/expamples/ant5/components/Statistic/index.tsx
@@ -0,0 +1,14 @@
+import { Statistic as AntdStatistic, StatisticProps } from "antd"
+import { forwardRef, memo } from "react"
+import { IconView } from "react-shells/ant5/components/IconView";
+import { IIcon } from "react-shells/ant5/components/IconView/model";
+
+export const Statistic = memo(forwardRef<HTMLDivElement, StatisticProps & { prefix: IIcon }>((props, ref) => {
+  const { prefix, ...other } = props;
+  return (
+    <AntdStatistic
+      prefix={prefix && <IconView icon={prefix} />}
+      {...other}
+    />
+  )
+}))
