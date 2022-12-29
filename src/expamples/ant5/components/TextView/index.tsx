a6c1df330729975f62f4f1fb81a1841e53a6a3e5
Water.Li <lizongbin@hotmail.com>
2022-12-29 16:36:45 +0800

    add textview

---
+++ src/expamples/ant5/components/TextView/index.tsx
@@ -0,0 +1,44 @@
+
+import React, { forwardRef, memo, useMemo } from "react";
+import cls from 'classnames'
+import dayjs from "dayjs";
+
+
+export enum TextType {
+  Number = "Number",
+  Date = "Date",
+  Text = "Text",
+}
+
+export interface ITextProps {
+  value?: string;
+  content?: string;
+  mode?: 'normal' | 'h1' | 'h2' | 'h3' | 'p';
+  style?: React.CSSProperties;
+  className?: string;
+  textType?: TextType;
+  formatMask?: string;
+}
+export const TextView = memo(forwardRef<HTMLDivElement>((props: ITextProps, ref) => {
+  const { value, textType = TextType.Text, formatMask, ...other } = props;
+
+  const tagName = props.mode === 'normal' || !props.mode ? 'div' : props.mode
+
+  const text = useMemo(() => {
+    const txtValue = props.content || value;
+    if (textType === TextType.Date) {
+      return dayjs(txtValue).format(formatMask || "YYYY-MM-DD HH:mm:ss");
+    }
+    return txtValue;
+  }, [formatMask, props.content, textType, value])
+  return React.createElement(
+    tagName,
+    {
+      ...other,
+      ref,
+      className: cls(props.className),
+      value: value,
+    },
+    text
+  )
+}))
