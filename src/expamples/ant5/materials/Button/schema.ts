759f496bdd7344d9d5ad52046f45001656dfc0e9
Water.Li <lizongbin@hotmail.com>
2022-12-29 12:06:35 +0800

    backup

--- src/expamples/ant5/materials/Button/schema.ts
+++ src/expamples/ant5/materials/Button/schema.ts
@@ -1,177 +1,79 @@
 import { INodeSchema } from "core";
-import { borderRediusSetter } from "react-shells/ant5/SettingsForm/schemas/borderRediusSetter";
-import { borderSetter } from "react-shells/ant5/SettingsForm/schemas/borderSetter";
-import { displaySetter } from "react-shells/ant5/SettingsForm/schemas/displaySetter";
-import { fontStyleSetter } from "react-shells/ant5/SettingsForm/schemas/fontStyleSetter";
-import { martinStyleSetter } from "react-shells/ant5/SettingsForm/schemas/martinStyleSetter";
-import { paddingStyleSetter } from "react-shells/ant5/SettingsForm/schemas/paddingStyleSetter";
+import { createSchema } from "react-shells/ant5/shared/createSchema";
 
-export const buttonSchema: INodeSchema = {
-  componentName: "Tabs",
-  props: {},
-  "x-field": {
-    type: "object",
-    name: "props",
-  },
-  children: [
+export const buttonSchema: INodeSchema = createSchema(
+  [
     {
-      componentName: "TabPanel",
+      componentName: "FormItem",
       props: {
-        title: "$properties"
+        label: "$title",
       },
       children: [
         {
-          componentName: "FormItem",
-          props: {
-            label: "$title",
+          componentName: "Input",
+          "x-field": {
+            name: "title",
           },
-          children: [
-            {
-              componentName: "Input",
-              "x-field": {
-                name: "title",
-              },
-            }
-          ]
-        },
-        {
-          componentName: "FormItem",
-          props: {
-            label: "$type",
-          },
-
-          children: [
-            {
-              componentName: "Select",
-              "x-field": {
-                name: "type",
-              },
-              props: {
-                options: [
-                  {
-                    value: 'primary',
-                    label: 'Primary',
-                  },
-                  {
-                    value: 'ghost',
-                    label: 'Ghost',
-                  },
-                  {
-                    value: 'dashed',
-                    label: 'Dashed',
-                  },
-                  {
-                    value: 'link',
-                    label: 'Link',
-                  },
-                  {
-                    value: 'text',
-                    label: 'Text',
-                  },
-                  {
-                    value: 'default',
-                    label: 'Default',
-                  },
-                ]
-              }
-            }
-          ]
-        },
-        {
-          componentName: "FormItem",
-          props: {
-            label: "$disabled",
-          },
-          children: [
-            {
-              componentName: "Switch",
-              "x-field": {
-                name: "disabled",
-                valuePropName: "checked",
-              },
-            }
-          ]
-        },
+        }
       ]
     },
     {
-      componentName: "TabPanel",
+      componentName: "FormItem",
       props: {
-        title: "$style"
+        label: "$type",
       },
-      "x-field": {
-        type: "object",
-        name: "style"
+
+      children: [
+        {
+          componentName: "Select",
+          "x-field": {
+            name: "type",
+          },
+          props: {
+            options: [
+              {
+                value: 'primary',
+                label: 'Primary',
+              },
+              {
+                value: 'ghost',
+                label: 'Ghost',
+              },
+              {
+                value: 'dashed',
+                label: 'Dashed',
+              },
+              {
+                value: 'link',
+                label: 'Link',
+              },
+              {
+                value: 'text',
+                label: 'Text',
+              },
+              {
+                value: 'default',
+                label: 'Default',
+              },
+            ]
+          }
+        }
+      ]
+    },
+    {
+      componentName: "FormItem",
+      props: {
+        label: "$disabled",
       },
       children: [
         {
-          componentName: "FormItem",
-          props: {
-            label: "$width",
+          componentName: "Switch",
+          "x-field": {
+            name: "disabled",
+            valuePropName: "checked",
           },
-          children: [
-            {
-              componentName: "SizeInput",
-              "x-field": {
-                name: "width",
-              },
-            }
-          ]
-        },
-        {
-          componentName: "FormItem",
-          props: {
-            label: "$height",
-          },
-          children: [
-            {
-              componentName: "SizeInput",
-              "x-field": {
-                name: "height",
-              },
-            }
-          ]
-        },
-        displaySetter,
-        {
-          componentName: "FormItem",
-          props: {
-            label: "$background",
-          },
-          children: [
-            {
-              componentName: "ColorInput",
-              "x-field": {
-                name: "backgroundColor",
-              },
-            }
-          ]
-        },
-        fontStyleSetter,
-        martinStyleSetter,
-        paddingStyleSetter,
-        borderRediusSetter,
-        borderSetter,
-        {
-          componentName: "FormItem",
-          props: {
-            label: "$opacity",
-          },
-          children: [
-            {
-              componentName: "Slider",
-              "x-field": {
-                name: "opacity",
-              },
-              props: {
-                max: 1,
-                step: 0.1,
-                defaultValue: 1,
-              }
-            }
-          ]
-        },
+        }
       ]
-    }
+    },
   ]
-}
+)
