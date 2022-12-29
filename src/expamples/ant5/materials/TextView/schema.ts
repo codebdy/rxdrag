90577d3d553d19c6e1b66456a214c68dc1675e96
Water.Li <lizongbin@hotmail.com>
2022-12-29 20:29:18 +0800

    backup

--- src/expamples/ant5/materials/TextView/schema.ts
+++ src/expamples/ant5/materials/TextView/schema.ts
@@ -6,71 +6,13 @@ export const textViewSchema: INodeSchema = createSchema(
     {
       componentName: "FormItem",
       props: {
-        label: "$title",
+        label: "$content",
       },
       children: [
         {
           componentName: "Input",
           "x-field": {
-            name: "title",
-          },
-        }
-      ]
-    },
-    {
-      componentName: "FormItem",
-      props: {
-        label: "$type",
-      },
-
-      children: [
-        {
-          componentName: "Select",
-          "x-field": {
-            name: "type",
-          },
-          props: {
-            options: [
-              {
-                value: 'primary',
-                label: 'Primary',
-              },
-              {
-                value: 'ghost',
-                label: 'Ghost',
-              },
-              {
-                value: 'dashed',
-                label: 'Dashed',
-              },
-              {
-                value: 'link',
-                label: 'Link',
-              },
-              {
-                value: 'text',
-                label: 'Text',
-              },
-              {
-                value: 'default',
-                label: 'Default',
-              },
-            ]
-          }
-        }
-      ]
-    },
-    {
-      componentName: "FormItem",
-      props: {
-        label: "$disabled",
-      },
-      children: [
-        {
-          componentName: "Switch",
-          "x-field": {
-            name: "disabled",
-            valuePropName: "checked",
+            name: "content",
           },
         }
       ]
