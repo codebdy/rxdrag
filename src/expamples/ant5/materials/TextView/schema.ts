135c6c573008faca04d1d72ae8b5b39bbc4c4e82
Water.Li <lizongbin@hotmail.com>
2022-12-31 10:40:58 +0800

    backup

--- src/expamples/ant5/materials/TextView/schema.ts
+++ src/expamples/ant5/materials/TextView/schema.ts
@@ -17,5 +17,49 @@ export const textViewSchema: INodeSchema = createSchema(
         }
       ]
     },
+    {
+      componentName: "FormItem",
+      props: {
+        label: "$mode",
+      },
+      children: [
+        {
+          componentName: "Select",
+          "x-field": {
+            name: "mode",
+          },
+          props: {
+            optionType: "button",
+            options: [
+              {
+                label: "Normal",
+                value: "Normal"
+              },
+              {
+                label: "H1",
+                value: "h1"
+              },
+              {
+                label: "H2",
+                value: "h2"
+              },
+              {
+                label: "H3",
+                value: "h3"
+              },
+              {
+                label: "H4",
+                value: "h4"
+              },
+              {
+                label: "p",
+                value: "p"
+              },
+            ],
+            defaultValue: "default",
+          }
+        }
+      ]
+    },
   ]
 )
