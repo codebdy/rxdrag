5ac64f9c0e71f55d6024f39a6fd0175bef224f0e
Water.Li <lizongbin@hotmail.com>
2022-12-31 16:47:32 +0800

    backup

--- src/expamples/ant5/materials/TextView/schema.ts
+++ src/expamples/ant5/materials/TextView/schema.ts
@@ -29,7 +29,6 @@ export const textViewSchema: INodeSchema = createSchema(
             name: "mode",
           },
           props: {
-            optionType: "button",
             options: [
               {
                 label: "Normal",
