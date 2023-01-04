9f0e3169a423ce1414918992213e4b3919b28180
Water.Li <lizongbin@hotmail.com>
2023-01-04 12:35:17 +0800

    Update index.tsx

--- src/react-shells/ant5/SettingsForm/components/ColInput/index.tsx
+++ src/react-shells/ant5/SettingsForm/components/ColInput/index.tsx
@@ -1,5 +1,5 @@
 import { Col, Form, Input, InputNumber, Row, Select } from "antd"
-import { memo, useCallback, useMemo, useState } from "react"
+import { ChangeEvent, memo, useCallback, useMemo, useState } from "react"
 import { Fold, FoldBase, FoldExtra } from "../Fold"
 import "./style.less"
 
@@ -44,14 +44,6 @@ export const ColInput = memo((props: ColInputProps) => {
     setScreen(val)
   }, []);
 
-  const handleSpanChange = useCallback((val: number | null) => {
-    if (screen === "all") {
-      onChange?.({ ...value, span: val || undefined })
-    } else {
-      onChange?.({ ...value, [screen]: { ...value?.[screen] as ColValue, span: val || undefined } })
-    }
-  }, [onChange, screen, value])
-
   const spanValue = useMemo(() => {
     if (screen === "all") {
       return value?.span
@@ -60,6 +52,94 @@ export const ColInput = memo((props: ColInputProps) => {
     }
   }, [screen, value])
 
+  const flexValue = useMemo(() => {
+    if (screen === "all") {
+      return value?.flex
+    } else {
+      return (value?.[screen] as ColValue)?.flex
+    }
+  }, [screen, value])
+
+  const offsetValue = useMemo(() => {
+    if (screen === "all") {
+      return value?.offset
+    } else {
+      return (value?.[screen] as ColValue)?.offset
+    }
+  }, [screen, value])
+
+  const orderValue = useMemo(() => {
+    if (screen === "all") {
+      return value?.order
+    } else {
+      return (value?.[screen] as ColValue)?.order
+    }
+  }, [screen, value])
+
+  const pullValue = useMemo(() => {
+    if (screen === "all") {
+      return value?.pull
+    } else {
+      return (value?.[screen] as ColValue)?.pull
+    }
+  }, [screen, value])
+
+  const pushValue = useMemo(() => {
+    if (screen === "all") {
+      return value?.push
+    } else {
+      return (value?.[screen] as ColValue)?.push
+    }
+  }, [screen, value])
+
+  const handleSpanChange = useCallback((val: number | null) => {
+    if (screen === "all") {
+      onChange?.({ ...value, span: val || undefined })
+    } else {
+      onChange?.({ ...value, [screen]: { ...value?.[screen] as ColValue, span: val || undefined } })
+    }
+  }, [onChange, screen, value])
+
+  const handleFlexChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
+    const val = e.target.value || undefined
+    if (screen === "all") {
+      onChange?.({ ...value, flex: val })
+    } else {
+      onChange?.({ ...value, [screen]: { ...value?.[screen] as ColValue, flex: val } })
+    }
+  }, [onChange, screen, value])
+
+  const handleOffsetChange = useCallback((val: number | null) => {
+    if (screen === "all") {
+      onChange?.({ ...value, offset: val || undefined })
+    } else {
+      onChange?.({ ...value, [screen]: { ...value?.[screen] as ColValue, offset: val || undefined } })
+    }
+  }, [onChange, screen, value])
+
+  const handleOrderChange = useCallback((val: number | null) => {
+    if (screen === "all") {
+      onChange?.({ ...value, order: val || undefined })
+    } else {
+      onChange?.({ ...value, [screen]: { ...value?.[screen] as ColValue, order: val || undefined } })
+    }
+  }, [onChange, screen, value])
+
+  const handlePullChange = useCallback((val: number | null) => {
+    if (screen === "all") {
+      onChange?.({ ...value, pull: val || undefined })
+    } else {
+      onChange?.({ ...value, [screen]: { ...value?.[screen] as ColValue, pull: val || undefined } })
+    }
+  }, [onChange, screen, value])
+
+  const handlePushChange = useCallback((val: number | null) => {
+    if (screen === "all") {
+      onChange?.({ ...value, push: val || undefined })
+    } else {
+      onChange?.({ ...value, [screen]: { ...value?.[screen] as ColValue, push: val || undefined } })
+    }
+  }, [onChange, screen, value])
   return (
     <Fold className="rx-col-input">
       <FoldBase title={title}>
@@ -86,27 +166,27 @@ export const ColInput = memo((props: ColInputProps) => {
           </Col>
           <Col span={12}>
             <Form.Item label={subTitles?.flex} labelAlign="right">
-              <Input value={(value?.[screen] as ColValue)?.flex} />
+              <Input value={flexValue} onChange={handleFlexChange} />
             </Form.Item>
           </Col>
           <Col span={12}>
             <Form.Item label={subTitles?.offset} labelAlign="right">
-              <InputNumber value={(value?.[screen] as ColValue)?.offset} />
+              <InputNumber value={offsetValue} onChange={handleOffsetChange} />
             </Form.Item>
           </Col>
           <Col span={12}>
             <Form.Item label={subTitles?.order} labelAlign="right">
-              <InputNumber value={(value?.[screen] as ColValue)?.order} />
+              <InputNumber value={orderValue} onChange={handleOrderChange} />
             </Form.Item>
           </Col>
           <Col span={12}>
             <Form.Item label={subTitles?.pull} labelAlign="right">
-              <InputNumber value={(value?.[screen] as ColValue)?.pull} />
+              <InputNumber value={pullValue} onChange={handlePullChange} />
             </Form.Item>
           </Col>
           <Col span={12}>
             <Form.Item label={subTitles?.push} labelAlign="right">
-              <InputNumber value={(value?.[screen] as ColValue)?.push} />
+              <InputNumber value={pushValue} onChange={handlePushChange} />
             </Form.Item>
           </Col>
         </Row>
