0036705c07bf9ef1a42ff6afe6e9d446597944a1
Water.Li <lizongbin@hotmail.com>
2023-01-12 08:37:29 +0800

    Update index.ts

--- src/expamples/ant5/materials/popups/Popover/index.ts
+++ src/expamples/ant5/materials/popups/Popover/index.ts
@@ -29,7 +29,7 @@ export const PopoverMaterial: IComponentMaterial = {
           title: {
             componentName: "TextView",
             props: {
-              content: "Delete the task",
+              content: "Popover Title",
             }
           },
           content: {
