8591cd94eb7be0bb59dab6d1dbe701d0ff5f2dc1
Water.Li <lizongbin@hotmail.com>
2023-01-14 16:19:39 +0800

    backup

--- src/react-shells/ant5/SettingsForm/components/FunctionsInput/index.tsx
+++ src/react-shells/ant5/SettingsForm/components/FunctionsInput/index.tsx
@@ -1,11 +1,41 @@
-import { Button } from "antd"
-import { memo } from "react"
+import { Button, Modal } from "antd"
+import { useToolsTranslate } from "core-react/hooks/useToolsTranslate";
+import { memo, useCallback, useState } from "react"
 
 export const FunctionsInput = memo((props: {
   title: string
 }) => {
   const { title, ...other } = props;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const t = useToolsTranslate()
+  const showModal = useCallback(() => {
+    setIsModalOpen(true);
+  }, []);
+
+  const handleOk = useCallback(() => {
+    setIsModalOpen(false);
+  }, []);
+
+  const handleCancel = useCallback(() => {
+    setIsModalOpen(false);
+  }, []);
+
   return (
-    <Button {...other}>{title}</Button>
+    <>
+      <Button {...other} onClick={showModal}>{title}</Button>
+      <Modal
+        title={title}
+        open={isModalOpen}
+        okText={t("confirm")}
+        cancelText={t("cancel")}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
+    </>
+
   )
 })
