bca33403402d6addf5aaa6d59c590848a4765990
Water.Li <lizongbin@hotmail.com>
2023-01-15 16:55:53 +0800

    backup

--- src/react-shells/ant5/SettingsForm/components/FunctionsInput/index.tsx
+++ src/react-shells/ant5/SettingsForm/components/FunctionsInput/index.tsx
@@ -1,10 +1,44 @@
-import { Button, Modal } from "antd"
-import { useToolsTranslate } from "core-react/hooks/useToolsTranslate";
+import { RedoOutlined, UndoOutlined } from "@ant-design/icons";
+import { Button, Modal, Space } from "antd"
+import { useToken } from "antd/es/theme/internal";
 import { memo, useCallback, useState } from "react"
+import { redoIcon, undoIcon } from "react-shells/ant5/icons";
 import styled from "styled-components";
 
 const SytledContent = styled.div`
-  height: calc(100vh - 200px);
+  height: calc(100vh - 100px);
+  display: flex;
+  border: solid 1px;
+`
+
+const LeftArea = styled.div`
+  width: 220px;
+  border-right: solid 1px;
+  padding: 8px;
+`
+
+const CenterArea = styled.div`
+  flex:1;
+  display: flex;
+  flex-flow: column;
+`
+
+const Toolbar = styled.div`
+  display: flex;
+  padding: 0 8px;
+  height: 40px;
+  align-items: center;
+  border-bottom: solid 1px;
+`
+
+const CanvasContainer = styled.div`
+  flex: 1
+`
+
+const PropertyBox = styled.div`
+  width: 220px;
+  border-left: solid 1px;
+  padding: 8px;
 `
 
 export const FunctionsInput = memo((props: {
@@ -12,13 +46,10 @@ export const FunctionsInput = memo((props: {
 }) => {
   const { title, ...other } = props;
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const t = useToolsTranslate()
+  const [, token] = useToken()
+
   const showModal = useCallback(() => {
     setIsModalOpen(true);
-  }, []);
-
-  const handleOk = useCallback(() => {
-    setIsModalOpen(false);
   }, []);
 
   const handleCancel = useCallback(() => {
@@ -32,17 +63,29 @@ export const FunctionsInput = memo((props: {
         title={title}
         open={isModalOpen}
         mask={false}
-        okText={t("confirm")}
-        cancelText={t("cancel")}
-        onOk={handleOk}
+        footer={false}
         onCancel={handleCancel}
         width={1199}
         centered
       >
-        <SytledContent>
-          <p>Some contents...</p>
-          <p>Some contents...</p>
-          <p>Some contents...</p>
+        <SytledContent style={{ borderColor: token.colorBorder }}>
+          <LeftArea style={{ borderColor: token.colorBorder }}>
+            left
+          </LeftArea>
+          <CenterArea>
+            <Toolbar style={{ borderColor: token.colorBorder }}>
+              <Space>
+                <Button shape="circle" type="text" icon={undoIcon}></Button>
+                <Button shape="circle" disabled type="text" icon={redoIcon}></Button>
+              </Space>
+            </Toolbar>
+            <CanvasContainer style={{ backgroundColor: token.colorBgContainer }}>
+              CanvasContainer
+            </CanvasContainer>
+          </CenterArea>
+          <PropertyBox style={{ borderColor: token.colorBorder }}>
+            right
+          </PropertyBox>
         </SytledContent>
       </Modal>
     </>
