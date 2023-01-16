669633e16f35ca28f65259ded2d128dff13fe3df
Water.Li <lizongbin@hotmail.com>
2023-01-16 11:56:51 +0800

    Update Members.tsx

--- src/react-shells/ant5/SettingsForm/components/ReactionsInput/ReactionsEditor/Members.tsx
+++ src/react-shells/ant5/SettingsForm/components/ReactionsInput/ReactionsEditor/Members.tsx
@@ -25,6 +25,10 @@ const ListItem = styled((props) => <Button type="text" {...props} />)`
   margin: 4px 0;
 `
 
+const methodIcon = <span role="img" className="anticon"><svg width='1em' height="1em" viewBox="0 0 24 24">
+   <path fill="currentColor" d="M12,7A5,5 0 0,1 17,12C17,14.42 15.28,16.44 13,16.9V21H11V16.9C8.72,16.44 7,14.42 7,12A5,5 0 0,1 12,7M12,9A3,3 0 0,0 9,12A3,3 0 0,0 12,15A3,3 0 0,0 15,12A3,3 0 0,0 12,9Z" />
+</svg>
+</span>
 export const Members = memo(() => {
   const t = useToolsTranslate()
   return (
@@ -45,6 +49,14 @@ export const Members = memo(() => {
         <Text type="secondary">{t("methods")}</Text>
         <Button size="small" type="text" icon={<PlusOutlined />}></Button>
       </Title>
+      <List>
+        <ListItem icon={methodIcon} type="default">
+          打开
+        </ListItem>
+        <ListItem icon={methodIcon}>
+          关闭
+        </ListItem>
+      </List>
       <Title>
         <Text type="secondary">{t("variables")}</Text>
         <Button size="small" type="text" icon={<PlusOutlined />}></Button>
