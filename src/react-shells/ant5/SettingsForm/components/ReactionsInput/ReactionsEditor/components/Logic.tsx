6c284e152b979cc2ad55d09eb778b27d4797d396
Water.Li <lizongbin@hotmail.com>
2023-01-22 19:24:04 +0800

    backup

--- src/react-shells/ant5/SettingsForm/components/ReactionsInput/ReactionsEditor/components/Logic.tsx
+++ src/react-shells/ant5/SettingsForm/components/ReactionsInput/ReactionsEditor/components/Logic.tsx
@@ -2,6 +2,7 @@ import { memo } from "react"
 import { useAddEdge } from "../hooks/edit-meta/useAddEdge"
 import { useAddNode } from "../hooks/edit-meta/useAddNode"
 import { useMovedNode } from "../hooks/edit-meta/useMovedNode"
+import { useArrowhead } from "../hooks/useArrowhead"
 import { useShowCells } from "../hooks/useShowCells"
 import { useTraceLining } from "../hooks/useTraceLining"
 
@@ -11,5 +12,6 @@ export const Logic = memo(()=>{
   useAddNode()
   useAddEdge()
   useMovedNode()
+  useArrowhead()
   return null
 })
---
+++ src/react-shells/ant5/SettingsForm/components/ReactionsInput/ReactionsEditor/hooks/useArrowhead.ts
@@ -0,0 +1,37 @@
+import { Cell } from "@antv/x6";
+import { useCallback, useEffect } from "react";
+import { useEditorState } from "./useEditorState";
+
+export function useArrowhead() {
+  const { graph, dispatch } = useEditorState()
+
+  const handleMouseEnter = useCallback(({ cell }: { cell: Cell }) => {
+    cell.addTools([
+      {
+        name: "source-arrowhead",
+      },
+      {
+        name: "target-arrowhead",
+        args: {
+          attrs: {
+            fill: "red",
+          },
+        },
+      },
+    ]);
+  }, [])
+
+  const handleMouseLeave = useCallback(({ cell }: { cell: Cell }) => {
+    cell.removeTools();
+  }, [])
+
+  useEffect(() => {
+    graph?.on("edge:mouseenter", handleMouseEnter);
+    graph?.on("edge:mouseleave", handleMouseLeave);
+
+    return ()=>{
+      graph?.off("edge:mouseenter", handleMouseEnter);
+      graph?.off("edge:mouseleave", handleMouseLeave);
+    }
+  }, [graph, handleMouseEnter, handleMouseLeave])
+}
