0e47bd758f622610cf31e5677e6c19b35028da99
Water.Li <lizongbin@hotmail.com>
2023-01-27 19:34:00 +0800

    backup

--- src/react-shells/ant5/SettingsForm/components/ReactionsInput/ReactionsEditor/components/Toolbar/index.tsx
+++ src/react-shells/ant5/SettingsForm/components/ReactionsInput/ReactionsEditor/components/Toolbar/index.tsx
@@ -1,6 +1,6 @@
 import { ZoomOutOutlined, ZoomInOutlined } from "@ant-design/icons"
 import { Button, Divider, Space } from "antd"
-import { memo } from "react"
+import { memo, useCallback } from "react"
 import { undoIcon, redoIcon } from "react-shells/ant5/icons"
 import styled from "styled-components"
 import { mapIcon } from "../../../../../../icons/reactions"
@@ -31,7 +31,10 @@ export const Toolbar = memo((
   }
 ) => {
   const { showMap, toggleShowMap } = props
-  const { selected } = useEditorState()
+  const { selected, graph } = useEditorState()
+  const handleRemove = useCallback(()=>{
+    selected && graph?.getCellById(selected)?.remove()
+  }, [graph, selected])
 
   return (
     <StyledToolbar>
@@ -44,6 +47,7 @@ export const Toolbar = memo((
           icon={<span role="img" className="anticon"><svg width="1rem" height="1rem" viewBox="0 0 24 24">
             <path fill="currentColor" d="M6,19A2,2 0 0,0 8,21H16A2,2 0 0,0 18,19V7H6V19M8,9H16V19H8V9M15.5,4L14.5,3H9.5L8.5,4H5V6H19V4H15.5Z" />
           </svg></span>}
+          onClick = {handleRemove}
         ></ToolbarButton>
       </Space>
       <ToobarCenter>
--- src/react-shells/ant5/SettingsForm/components/ReactionsInput/ReactionsEditor/hooks/useSelection.ts
+++ src/react-shells/ant5/SettingsForm/components/ReactionsInput/ReactionsEditor/hooks/useSelection.ts
@@ -7,8 +7,13 @@ export function useSelection() {
   const { graph, dispatch } = useEditorState()
 
   const handleSelected = useCallback(({ cell }: { cell: Cell }) => {
-    dispatch({ type: ActionType.SELECTION, payload: cell.id })
-  }, [dispatch])
+    if (!graph?.getCellById(cell.id)) {
+      graph?.cleanSelection()
+      dispatch({ type: ActionType.SELECTION, payload: undefined })
+    } else {
+      dispatch({ type: ActionType.SELECTION, payload: cell.id })
+    }
+  }, [dispatch, graph])
 
   const handleUnSelected = useCallback(({ cell }: { cell: Cell }) => {
     dispatch({ type: ActionType.SELECTION, payload: undefined })
