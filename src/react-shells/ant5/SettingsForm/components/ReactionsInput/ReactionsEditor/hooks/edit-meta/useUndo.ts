d8239bdd70b0ab524372a8d71f3d5edaf4263a7b
Water.Li <lizongbin@hotmail.com>
2023-01-28 09:48:32 +0800

    can redo and undo

--- src/react-shells/ant5/SettingsForm/components/ReactionsInput/ReactionsEditor/hooks/edit-meta/useUndo.ts
+++ src/react-shells/ant5/SettingsForm/components/ReactionsInput/ReactionsEditor/hooks/edit-meta/useUndo.ts
@@ -3,7 +3,7 @@ import { ActionType } from "../../actions"
 import { useEditorState } from "../useEditorState"
 
 export function useUndo() {
-  const { undoList, redoList, dispatch } = useEditorState()
+  const { undoList, redoList, dispatch, selected, metas } = useEditorState()
 
   const undo = useCallback(() => {
     const snapshot = undoList[undoList.length - 1]
@@ -11,8 +11,8 @@ export function useUndo() {
     dispatch({ type: ActionType.SELECTION, payload: snapshot.selected })
     dispatch({ type: ActionType.SET_METAS, payload: { reactions, invokes } })
     dispatch({ type: ActionType.SET_UNOLIST, payload: undoList.slice(0, undoList.length - 1) })
-    dispatch({ type: ActionType.SET_REDOLIST, payload: [...redoList, snapshot] })
-  }, [dispatch, redoList, undoList])
+    dispatch({ type: ActionType.SET_REDOLIST, payload: [...redoList, {selected, ...metas}] })
+  }, [dispatch, metas, redoList, selected, undoList])
 
   return undo
 }
