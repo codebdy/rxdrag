47fded49059a791815a6ee118e0c16b2b321a3c7
Water.Li <lizongbin@hotmail.com>
2023-01-28 15:12:18 +0800

    Update index.tsx

--- src/react-shells/ant5/SettingsForm/components/ReactionsInput/index.tsx
+++ src/react-shells/ant5/SettingsForm/components/ReactionsInput/index.tsx
@@ -1,12 +1,15 @@
-import { Button, Form, Input, Modal, Row, Switch } from "antd"
+import { Button, Form, Input, Modal, Switch } from "antd"
 import { useToolsTranslate } from "core-react/hooks/useToolsTranslate";
 import { memo, useCallback, useEffect, useState } from "react"
+import { IControllerMeta } from "runner/reaction/interfaces/metas";
 import { ReactionsEditor } from "./ReactionsEditor";
 
 export const ReactionsInput = memo((props: {
   title: string,
+  value?: IControllerMeta,
+  onChange?: (value?: IControllerMeta) => void,
 }) => {
-  const { title, ...other } = props;
+  const { title, value, onChange, ...other } = props;
   const [isModalOpen, setIsModalOpen] = useState(false);
   const t = useToolsTranslate()
 
@@ -29,35 +32,39 @@ export const ReactionsInput = memo((props: {
       >
         <Switch />
       </Form.Item>
-      <>
-        <Form.Item
-          label={t("controllerName")}
-        >
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label={t("config")}
-        >
-          <Button {...other} onClick={showModal}>{t("configController")}</Button>
-        </Form.Item>
+      {
+        value?.id &&
+        <>
+          <Form.Item
+            label={t("controllerName")}
+          >
+            <Input />
+          </Form.Item>
+          <Form.Item
+            label={t("config")}
+          >
+            <Button {...other} onClick={showModal}>{t("configController")}</Button>
+          </Form.Item>
 
-        <Modal
-          title={`${t("configController")} - 对话框`}
-          open={isModalOpen}
-          cancelText={t("cancel")}
-          okText={t("confirm")}
-          onCancel={handleCancel}
-          width={"calc(100vw - 40px)"}
-          style={{
-            transform: 'scale(1)',
-          }}
-          getContainer={false}
-          centered
-          destroyOnClose
-        >
-          <ReactionsEditor />
-        </Modal>
-      </>
+          <Modal
+            title={`${t("configController")} - 对话框`}
+            open={isModalOpen}
+            cancelText={t("cancel")}
+            okText={t("confirm")}
+            onCancel={handleCancel}
+            width={"calc(100vw - 40px)"}
+            style={{
+              transform: 'scale(1)',
+            }}
+            getContainer={false}
+            centered
+            destroyOnClose
+          >
+            <ReactionsEditor />
+          </Modal>
+        </>
+      }
+
     </div>
 
   )
