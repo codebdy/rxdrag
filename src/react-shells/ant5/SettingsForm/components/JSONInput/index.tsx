fc9458a02a760652de214a4b1669f4f77ebb62c9
Water.Li <lizongbin@hotmail.com>
2023-02-09 18:15:40 +0800

    Update index.tsx

--- src/react-shells/ant5/SettingsForm/components/JSONInput/index.tsx
+++ src/react-shells/ant5/SettingsForm/components/JSONInput/index.tsx
@@ -1,13 +1,48 @@
 import { EditOutlined } from "@ant-design/icons"
-import { Button } from "antd"
+import Editor from "@monaco-editor/react"
+import { Button, Drawer, Space } from "antd"
+import { useThemeMode } from "core-react/hooks/useThemeMode"
 import { useToolsTranslate } from "core-react/hooks/useToolsTranslate"
-import { memo } from "react"
+import { memo, useCallback, useState } from "react"
 
 export const JSONInput = memo(() => {
+  const [open, setOpen] = useState(false);
+  const themeMode = useThemeMode()
+  
   const t = useToolsTranslate()
+  const showDrawer = useCallback(() => {
+    setOpen(true);
+  }, []);
+
+  const handleClose = useCallback(() => {
+    setOpen(false);
+  }, []);
   return (
     <>
-      <Button icon={<EditOutlined />}>{t('edit')}</Button>
+      <Button icon={<EditOutlined />} onClick={showDrawer}>{t('edit')}</Button>
+      <Drawer
+        title="Basic Drawer"
+        placement="right"
+        mask={false}
+        onClose={handleClose}
+        open={open}
+        size="large"
+        extra={
+          <Space>
+            <Button onClick={handleClose}>{t("cancel")}</Button>
+            <Button type="primary" onClick={handleClose}>
+              {t("confirm")}
+            </Button>
+          </Space>
+        }
+      >
+        <Editor
+          height="100%"
+          language="json"
+          theme={themeMode === "dark" ? "vs-dark" : "vs-light"}
+          value={""}
+        />
+      </Drawer>
     </>
   )
 })
