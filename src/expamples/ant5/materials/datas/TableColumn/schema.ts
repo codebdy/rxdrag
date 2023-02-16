5370d62c44845dfd6361da3e2bd9055bc1e1aedf
Water.Li <lizongbin@hotmail.com>
2023-02-16 17:20:46 +0800

    Update schema.ts

--- src/expamples/ant5/materials/datas/TableColumn/schema.ts
+++ src/expamples/ant5/materials/datas/TableColumn/schema.ts
@@ -1,26 +1,19 @@
 import { INodeSchema } from "core";
-import { createSchema, SchemaOptions } from "react-shells/ant5/shared/createSchema";
+import { createSchema, SchemaOptions, withFormItem } from "react-shells/ant5/shared/createSchema";
 
 const options: SchemaOptions = {
   propsSchemas: [
     {
-      componentName: "FormItem",
-      props: {
+      componentName: "Input",
+      "x-field": {
+        name: "title",
         label: "$title",
-      },
-      children: [
-        {
-          componentName: "Input",
-          "x-field": {
-            name: "title",
-            params: {
-              withBind: true,
-            }
-          },
+        params: {
+          withBind: true,
         }
-      ],
-    },
+      },
+    }
   ]
 }
 
-export const materialSchema: INodeSchema = createSchema(options)
+export const materialSchema: INodeSchema = createSchema(withFormItem(options))
