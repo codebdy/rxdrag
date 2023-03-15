9d748d512b9a37a920c5b379577dcb64a2059b2e
Water.Li <lizongbin@hotmail.com>
2023-03-15 13:34:56 +0800

    backup

---
+++ packages/react-fieldy/rollup.config.mjs
@@ -0,0 +1,34 @@
+import typescript from "rollup-plugin-typescript2";
+import external from "rollup-plugin-peer-deps-external";
+import less from 'rollup-plugin-less';
+
+import pkg from "./package.json" assert { type: "json" };
+
+export default {
+  input: "src/index.ts",
+  output: [
+    {
+      file: pkg.main,
+      format: "cjs",
+      exports: "named",
+      sourcemap: true,
+      strict: false,
+    },
+  ],
+  external: [
+    "@reduxjs/toolkit", 
+    "redux", 
+    "@rxdrag/shared", 
+    "lodash", 
+    "react",
+    "react-dom/client",
+    "@rxdrag/react-shared"
+  ],
+  plugins: [
+    less({insert:true}),
+    // Exclude peer dependencies from the bundle
+    external(),
+    // Compile TypeScript files
+    typescript()
+  ],
+};
