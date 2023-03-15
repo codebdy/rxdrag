5ae6efc190f7f871ff6c0d848cf7721f92b0dfc2
Water.Li <lizongbin@hotmail.com>
2023-03-15 17:10:00 +0800

    react-fieldy可以成功编译

--- packages/react-fieldy/src/contexts.ts
+++ packages/react-fieldy/src/contexts.ts
@@ -1,5 +1,5 @@
+import { IField, IFieldyEngine, IForm } from "@rxdrag/fieldy";
 import { createContext } from "react";
-import { IField, IFieldyEngine, IForm } from "../../fieldy/src/interfaces";
 
 export const FieldyContext = createContext<IFieldyEngine | undefined>(undefined)
 export const FormContext = createContext<IForm | undefined>(undefined)
