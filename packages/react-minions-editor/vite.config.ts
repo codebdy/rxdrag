cac276e30815385d748114a0755f5502f5d4eb76
Water.Li <lizongbin@hotmail.com>
2023-03-19 22:11:55 +0800

    backup

--- packages/react-minions-editor/vite.config.ts
+++ packages/react-minions-editor/vite.config.ts
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  // Configuration for building your library.
+  // See: https://vitejs.dev/guide/build.html#library-mode
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points.
+      entry: 'src/index.ts',
+      name: 'RxdragMinionsEditor',
+      fileName: 'index',
+      // Change this to the formats you want to support.
+      // Don't forgot to update your package.json as well.
+      formats: ['es', 'cjs']
+    },
+    rollupOptions: {
+      // External packages that should not be bundled into your library.
+      external: []
+    }
+  }
+});
