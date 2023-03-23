c87324aba6c03f2708eb370cc587fa13859c4827
Water.Li <lizongbin@hotmail.com>
2023-03-23 09:12:25 +0800

    Update index.ts

--- packages/react-antd-materials/src/materials/datas/Table/index.ts
+++ packages/react-antd-materials/src/materials/datas/Table/index.ts
@@ -1,5 +1,5 @@
+import { Table } from "@rxdrag/react-antd-components";
 import { IComponentMaterial } from "@rxdrag/react-core";
-import { Table } from "antd";
 import { FooterMaterial } from "../../common/Footer";
 import { HeaderMaterial } from "../../common/Header";
 import { ListItemMaterial } from "../ListItem";
