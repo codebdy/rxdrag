26f86f426bad8e343192c270e71d62289610abd7
qingtong <qingtong@zhejianglab.com>
2023-04-15 21:58:22 +0800

    feat: iframe 加载成功触发页面渲染

--- packages/react-core/src/canvas/IframeCanvas/IFrameCanvasRender.tsx
+++ packages/react-core/src/canvas/IframeCanvas/IFrameCanvasRender.tsx
@@ -1,6 +1,6 @@
 import { IDesignerEngine, IDocument } from "@rxdrag/core";
 import { IComponents } from "@rxdrag/react-shared";
-import React from "react"
+import React, { useState } from "react"
 import { memo } from "react"
 import { CanvasRender } from "../CanvasRender"
 
@@ -10,11 +10,22 @@ export const IFrameCanvasRender = memo((props: {
   designers: IComponents
 }) => {
   const {designers}= props
+  const [ready, setReady] = useState(false);
   const engine = window.engine
   const doc = window.doc
 
+  function receiveMessageFromIndex(event: MessageEvent) {
+    // 监听父窗口 ready 事件
+    if (event.data === 'iframeReady') {
+      console.log('RXDrag: iframeReady');
+      setReady(true);
+    }
+  }
+
+  window.addEventListener('message', receiveMessageFromIndex, false);
+
   return (
-    doc ?
+    doc && engine && !!ready ?
       <CanvasRender engine={engine} doc={doc} components={designers} />
       : null
   )
