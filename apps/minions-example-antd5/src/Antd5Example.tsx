5646b9761cbf2fa16beb7ae9250cdb46c6a6abd3
Water.Li <lizongbin@hotmail.com>
2023-05-17 13:40:25 +0800

    backup

---
+++ apps/minions-example-antd5/src/Antd5Example.tsx
@@ -0,0 +1,17 @@
+import { memo } from "react"
+
+export enum LeftNavType {
+  pages = "pages",
+  compoents = "components",
+  outline = "outline",
+  history = "history",
+}
+
+export const Antd5Example = memo(() => {
+
+  return (
+    <div>
+      逻辑编排引擎
+    </div>
+  )
+})
