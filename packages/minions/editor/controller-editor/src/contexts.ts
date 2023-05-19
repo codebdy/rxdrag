e19978e68ae46fa5ce29f265ae6863c2c8d66bc0
Water.Li <lizongbin@hotmail.com>
2023-05-19 15:48:52 +0800

    Update contexts.ts

--- packages/minions/editor/controller-editor/src/contexts.ts
+++ packages/minions/editor/controller-editor/src/contexts.ts
@@ -1,3 +1,4 @@
+import { IControllerMeta } from "@rxdrag/minions-runtime-react"
 import { createContext } from "react"
 
 export const ControllerContext = createContext<IControllerMeta | undefined>(undefined)
