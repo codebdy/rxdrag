d5b1739aaf5f55d154711568d62da01cc3ac6a4e
Water.Li <lizongbin@hotmail.com>
2023-05-31 20:33:41 +0800

    Update index.ts

--- packages/minions-antd5/minions-react-materials/src/controller/index.ts
+++ packages/minions-antd5/minions-react-materials/src/controller/index.ts
@@ -1,9 +1,14 @@
-import { IActivityMaterial } from "@rxdrag/minions-schema";
-import { ReactNode } from "react";
 import { setPropMaterial } from "./setProp";
 import { setVariableMaterial } from "./setVariable";
+import { IRxDragActivityMaterial } from "../interfaces";
+import { IControllerEditorContextParam } from "@rxdrag/minions-controller-editor";
+import { readVariableMaterial } from "./readVariable";
+import { listenVariableMaterial } from "./listenVariable";
 
-export const controllerActivites: IActivityMaterial<ReactNode>[] = [
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const controllerActivites: IRxDragActivityMaterial<any, IControllerEditorContextParam>[] = [
   setPropMaterial,
   setVariableMaterial,
+  listenVariableMaterial,
+  readVariableMaterial,
 ]
