5b130dcbe27a045a2a2dc0f376c4066fa2c85a4d
Water.Li <lizongbin@hotmail.com>
2023-06-01 12:32:58 +0800

    Update useUpdateNode.ts

--- packages/minions/editor/logicflow-editor/src/hooks/useUpdateNode.ts
+++ packages/minions/editor/logicflow-editor/src/hooks/useUpdateNode.ts
@@ -17,13 +17,21 @@ export function useUpdateNode() {
       graphNode.setPosition(nodeMeta.x6Node);
       const subLabel = getSubLabel(nodeMeta)
       const height = getHeight(nodeMeta, !!subLabel)
-      graphNode.replaceData({ ...graphNode.data, meta: nodeMeta, subLabel, height: height })
+      const ports = transPorts(nodeMeta)
+      graphNode.replaceData({ 
+        ...graphNode.data, 
+        meta: nodeMeta, 
+        subLabel, 
+        height: height,
+        inputCounts: nodeMeta.inPorts?.length || ports.filter(port => port.group === 'in').length,
+        outputCounts: nodeMeta.outPorts?.length || ports.filter(port => port.group === 'out').length,
+       })
       if (nodeMeta.type === ActivityType.Start || nodeMeta.type === ActivityType.End) {
         graphNode.attr("text/text", nodeMeta.label)
       } else {
         graphNode.setSize({ ...nodeMeta.x6Node, width: getNodeWidth(nodeMeta, subLabel), height: height });
         const oldPorts = graphNode.getPorts()
-        const ports = transPorts(nodeMeta)
+        //const ports = transPorts(nodeMeta)
         for (const port of ports || []) {
           if (oldPorts.find(prt => prt.id === port.id)) {
             graphNode.portProp(port.id, 'attrs/text/text', port.attrs.text.text)
