46193645246e4303ea9a61e9bc58c0da8a877630
Water.Li <lizongbin@hotmail.com>
2023-07-02 12:10:26 +0800

    Update SubmitController.ts

--- apps/example-antd5/src/controller/shortcuts/SubmitController.ts
+++ apps/example-antd5/src/controller/shortcuts/SubmitController.ts
@@ -2,6 +2,7 @@ import { AbstractController, INIT_EVENT_NAME } from "@rxdrag/minions-runtime-rea
 import { IShortcutControllerMeta } from "./IShortcutControllerMeta";
 import { SUBMIT_CONTROLLER_NAME } from "./consts";
 
+//该控制器最好结合Page，页面模块做好再实现
 export class SubmitController extends AbstractController {
   constructor(public meta: IShortcutControllerMeta) {
     super(meta)
