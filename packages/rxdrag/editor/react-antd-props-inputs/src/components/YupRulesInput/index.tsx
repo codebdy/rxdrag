d47dbeff5f8b768c90993b674a2fcd2e166d1d4b
Water.Li <lizongbin@hotmail.com>
2023-07-26 12:17:20 +0800

    backup

--- packages/rxdrag/editor/react-antd-props-inputs/src/components/YupRulesInput/index.tsx
+++ packages/rxdrag/editor/react-antd-props-inputs/src/components/YupRulesInput/index.tsx
@@ -1,4 +1,4 @@
-import { Form } from "antd"
+import { Form, Select } from "antd"
 import { memo } from "react"
 import { YupValidateRules } from "@rxdrag/fieldy-yup-validation"
 import { useSettersTranslate } from "@rxdrag/react-core"
@@ -14,7 +14,20 @@ export const YupRulesInput = memo((
   return (
     <>
       <Form.Item label={t('validationType')}>
-
+        <Select
+          allowClear
+          options={[
+            { value: 'email', label: t('email') },
+            { value: 'url', label: t('url') },
+            { value: 'uuid', label: t('UUID') },
+            { value: 'string', label: t('string') },
+            { value: 'number', label: t('number') },
+            { value: 'date', label: t('date') },
+            { value: 'boolean', label: t('boolean') },
+            { value: 'array', label: t('array') },
+            { value: 'object', label: t('object') },
+          ]}
+        />
       </Form.Item>
     </>
   )
--- packages/rxdrag/editor/react-antd-props-inputs/src/locales.ts
+++ packages/rxdrag/editor/react-antd-props-inputs/src/locales.ts
@@ -3,9 +3,25 @@ import { ILocales } from "@rxdrag/locales";
 export const setterLocales: ILocales = {
   "zh-CN": {
     validationType: "校验类型",
+    email: "邮箱",
+    url: "网址",
+    string: "字符串",
+    number: "数字",
+    date: "日期",
+    boolean: "布尔",
+    array: "数组",
+    object: "对象",
 
   },
   'en-US': {
     validationType: "Validation Type",
+    email: "Email",
+    url: "Url",
+    string: "String",
+    number: "Number",
+    date: "Date",
+    boolean: "Boolean",
+    array: "Array",
+    object: "Object",
   }
 }
