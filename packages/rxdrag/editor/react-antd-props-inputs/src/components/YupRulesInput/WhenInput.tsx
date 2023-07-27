b60a054519dab280dc90b40a63b8737611e0b5ed
Water.Li <lizongbin@hotmail.com>
2023-07-27 11:49:24 +0800

    backup

--- packages/rxdrag/editor/react-antd-props-inputs/src/components/YupRulesInput/WhenInput.tsx
+++ packages/rxdrag/editor/react-antd-props-inputs/src/components/YupRulesInput/WhenInput.tsx
@@ -1,14 +1,50 @@
 import { useSettersTranslate } from "@rxdrag/react-core"
-import { Button, Form } from "antd"
-import { memo } from "react"
+import { Button, Form, Modal } from "antd"
+import { memo, useCallback, useState } from "react"
+import styled from "styled-components"
+import DepTable from "./DepTable"
+
+const Content = styled.div`
+  display: flex;
+  flex-flow: column;
+`
 
 export const WhenInput = memo(() => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const t = useSettersTranslate()
+  const handleShow = useCallback(() => {
+    setIsModalOpen(true);
+  }, []);
+
+  const handleOk = useCallback(() => {
+    setIsModalOpen(false);
+  }, []);
+
+  const handleCancel = useCallback(() => {
+    setIsModalOpen(false);
+  }, []);
+
   return (
     <>
       <Form.Item label={t("customized")}>
-        <Button>{t("configRules")}</Button>
+        <Button onClick={handleShow}>{t("configRules")}</Button>
       </Form.Item>
+      <Modal
+        title={t("configRules")}
+        open={isModalOpen}
+        okText={t("confirm")}
+        cancelText={t("cancel")}
+        width="600px"
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <DepTable />
+        <div>
+          配置代码
+        </div>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
     </>
   )
 })
