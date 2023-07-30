3509effbf4a5ae64d63c33632f8d3ea5b15e24b9
Water.Li <lizongbin@hotmail.com>
2023-07-30 19:44:58 +0800

    Update index.tsx

--- packages/rxdrag/runtime/react-antd-components/src/forms/FormItem/index.tsx
+++ packages/rxdrag/runtime/react-antd-components/src/forms/FormItem/index.tsx
@@ -1,21 +1,26 @@
-import React, { useContext } from "react"
+import React, { useContext, useMemo } from "react"
 import { Form, FormItemProps } from "antd";
 import { memo } from "react";
 import { FormLayoutContext } from "../contexts";
+import { isArray } from "lodash";
 
 export const FormItem: React.FC<{
-  children?: React.ReactNode
+  value?: unknown,
+  onChange?: (value?: unknown) => void,
+  children?: React.ReactElement
 } & FormItemProps> = memo((props) => {
-  const { children, ...other } = props
+  const { value, onChange, children, ...other } = props
 
   const formParams = useContext(FormLayoutContext);
-
+  const child = useMemo(() => {
+    return isArray(children) ? children?.[0] : children
+  }, [children])
   return (
     <Form.Item
       {...formParams}
       {...other}
     >
-      {children}
+      {child && React.cloneElement(child, { value, onChange })}
     </Form.Item>
   )
 })
