82f076fb651d449120e110af81bb635be7586e13
Water.Li <lizongbin@hotmail.com>
2023-08-01 12:25:50 +0800

    add styled.d.ts

--- packages/minions/editor/logicflow-editor/src/interfaces/index.ts
+++ packages/minions/editor/logicflow-editor/src/interfaces/index.ts
@@ -1,3 +1,4 @@
 export * from "./interfaces"
 export * from "./state"
 export * from "./theme"
+export * from "./styled"
---
+++ packages/minions/editor/logicflow-editor/src/interfaces/styled.d.ts
@@ -0,0 +1,11 @@
+// import original module declarations
+import 'styled-components';
+import { IThemeToken } from './theme';
+
+//@@需要跟其他模块统一
+// and extend them!
+declare module 'styled-components' {
+  export interface DefaultTheme {
+    token?: IThemeToken
+  }
+}
