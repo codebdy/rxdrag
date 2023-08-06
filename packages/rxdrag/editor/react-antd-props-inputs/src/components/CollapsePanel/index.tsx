1c28cb3404d4b6e99a98a56b9273f2baa9d5e6b9
Water.Li <lizongbin@hotmail.com>
2023-08-06 16:43:16 +0800

    Update index.tsx

--- packages/rxdrag/editor/react-antd-props-inputs/src/components/CollapsePanel/index.tsx
+++ packages/rxdrag/editor/react-antd-props-inputs/src/components/CollapsePanel/index.tsx
@@ -2,8 +2,6 @@ import React from "react"
 import { Collapse as AntdCollapse } from "antd"
 import { memo } from "react"
 
-const { Panel } = AntdCollapse;
-
 export const CollapsePanel = memo((
   props: {
     title?: string,
@@ -16,10 +14,18 @@ export const CollapsePanel = memo((
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { title, children, defaultExpand, onChange, ...other } = props;
   return (
-    <AntdCollapse ghost defaultActiveKey={defaultExpand ? ['1'] : undefined} expandIconPosition="end" {...other}>
-      <Panel header={title} key="1" forceRender>
-        {children}
-      </Panel>
-    </AntdCollapse>
+    <AntdCollapse
+      ghost defaultActiveKey={defaultExpand ? ['1'] : undefined}
+      expandIconPosition="end"
+      items={[
+        {
+          key : "1",
+          label: title,
+          children:children,
+          forceRender: true,
+        }
+      ]}
+      {...other}
+    />
   )
 })
