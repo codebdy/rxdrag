4eb78cc3eae58eb863935ea764b8510ceab8ef64
Water.Li <lizongbin@hotmail.com>
2023-08-19 18:33:25 +0800

    Update ResizeHandlers.tsx

--- packages/rxdrag/editor/react-antd-shell-inline/src/components/DraggableWidget/ResizeHandlers.tsx
+++ packages/rxdrag/editor/react-antd-shell-inline/src/components/DraggableWidget/ResizeHandlers.tsx
@@ -1,54 +1,80 @@
 import { memo } from "react"
 import styled from "styled-components"
 
-const width = 6;
+const thickness = 6;
+
+const offset = -(thickness / 2);
 
 const Handler = styled.div`
   position: absolute;
-  background-color: #fff;
-  border:solid 1px red;
   z-index: 1;
 `
-
 const HorizontalHandler = styled(Handler)`
-  width: calc(100% - ${width * 2}px);
+  width: calc(100% - ${thickness * 2}px);
   //左侧
-  left: ${width}px;
-  height: ${width}px;
+  left: ${thickness}px;
+  height: ${thickness}px;
+  cursor: s-resize;
 `
-
 const VerticalHandler = styled(Handler)`
-  height: calc(100% - ${width * 2}px);
-  width: ${width}px;
+  height: calc(100% - ${thickness * 2}px);
+  width: ${thickness}px;
   //顶
-  top: ${width}px;
+  top: ${thickness}px;
+  cursor: e-resize;
 `
-
+const CornerHandler = styled(Handler)`
+  width: ${thickness * 2}px;
+  height: ${thickness * 2}px;
+  z-index: 2;
+`
+const LeftTopHandler = styled(CornerHandler)`
+  left: ${offset}px;
+  top: ${offset}px;
+  cursor: nw-resize;
+`
+const RightTopHandler = styled(CornerHandler)`
+  right: ${offset}px;
+  top: ${offset}px;
+  cursor: ne-resize;
+`
+const RightBottomHandler = styled(CornerHandler)`
+  bottom: ${offset}px;
+  right: ${offset}px;
+  cursor: nw-resize;
+`
+const LeftBottomHandler = styled(CornerHandler)`
+  left: ${offset}px;
+  bottom: ${offset}px;
+  cursor: ne-resize;
+`
 const RightHandler = styled(VerticalHandler)`
   //偏移
-  right:${-(width / 2)}px;
+  right:${offset}px;
 `
-
 const LeftHandler = styled(VerticalHandler)`
   //偏移
-  left:${-(width / 2)}px;
+  left:${offset}px;
 `
-
 const TopHandler = styled(HorizontalHandler)`
   //偏移
-  top: ${-(width / 2)}px;  
+  top: ${offset}px;  
 `
 const BottomHandler = styled(HorizontalHandler)`
   //偏移
-  bottom: ${-(width / 2)}px;  
+  bottom: ${offset}px;  
 `
 export const ResizeHandlers = memo(() => {
   return (
     <>
       <LeftHandler />
+      <LeftTopHandler />
       <TopHandler />
+      <RightTopHandler />
       <RightHandler />
+      <RightBottomHandler />
       <BottomHandler />
+      <LeftBottomHandler />
     </>
   )
 })
