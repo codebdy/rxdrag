7f0d694aac3822f5bcefeb2bf0782f1e7e90288d
Water.Li <lizongbin@hotmail.com>
2023-08-30 18:33:28 +0800

    backup

---
+++ packages/rxdrag/editor/react-antd-shell/src/zoomable/DocView/DocViewInner.tsx
@@ -0,0 +1,62 @@
+import { memo } from "react"
+import styled from "styled-components"
+import { DeleteOutlined } from "@ant-design/icons"
+import { Button, Space } from "antd"
+import { Spring } from "../../common"
+import { useDocument } from "@rxdrag/react-core"
+
+const DocViewContainer = styled.div`
+  position: relative;
+  width: 1200px;
+  .actions{
+    display: none;
+  }
+
+  &:hover{
+    .canvas-content{
+      outline: solid 1px ${props => props.theme.token?.colorPrimary};
+    }
+    .actions{
+      display: flex;
+    }
+  }
+`
+
+const CanvasToolbar = styled.div`
+  display: flex;
+  height: 32px;
+  padding: 0px;
+  color: ${props => props.theme.token?.colorText};
+  //border-bottom: solid 1px ${props => props.theme.token?.colorBorder};
+  //background-color: ${props => props.theme.token?.colorBorderSecondary};
+  align-items: center;
+`
+
+const CanvasContent = styled.div`
+  width: 1200px;
+  height: 600px;
+  background-color: white;
+`
+
+const CanvasTitle = styled.span`
+  color:${props => props.theme.token?.colorTextSecondary};
+  font-size: 13px;
+`
+
+export const DocViewInner = memo(() => {
+  const doc = useDocument()
+  return (
+    <DocViewContainer>
+      <CanvasToolbar>
+        <CanvasTitle>
+          {doc?.getTitle()} - <em>大屏</em>
+        </CanvasTitle>
+        <Spring />
+        <Space className="actions">
+          <Button type="text" size="small" shape="circle" icon={<DeleteOutlined />} />
+        </Space>
+      </CanvasToolbar>
+      <CanvasContent className="canvas-content" />
+    </DocViewContainer>
+  )
+})
--- packages/rxdrag/editor/react-antd-shell/src/zoomable/DocView/index.tsx
+++ packages/rxdrag/editor/react-antd-shell/src/zoomable/DocView/index.tsx
@@ -1,47 +1,8 @@
 import { memo } from "react"
-import styled from "styled-components"
-import { DeleteOutlined } from "@ant-design/icons"
-import { Button, Space } from "antd"
-import { Spring } from "../../common"
 import { IDocument } from "@rxdrag/core"
+import { DocViewInner } from "./DocViewInner"
+import { DocumentRoot } from "@rxdrag/react-core"
 
-const DocViewContainer = styled.div`
-  position: relative;
-  width: 1200px;
-  .actions{
-    display: none;
-  }
-
-  &:hover{
-    .canvas-content{
-      outline: solid 1px ${props => props.theme.token?.colorPrimary};
-    }
-    .actions{
-      display: flex;
-    }
-  }
-`
-
-const CanvasToolbar = styled.div`
-  display: flex;
-  height: 32px;
-  padding: 0px;
-  color: ${props => props.theme.token?.colorText};
-  //border-bottom: solid 1px ${props => props.theme.token?.colorBorder};
-  //background-color: ${props => props.theme.token?.colorBorderSecondary};
-  align-items: center;
-`
-
-const CanvasContent = styled.div`
-  width: 1200px;
-  height: 600px;
-  background-color: white;
-`
-
-const CanvasTitle = styled.span`
-  color:${props => props.theme.token?.colorTextSecondary};
-  font-size: 13px;
-`
 
 export const DocView = memo((
   props: {
@@ -50,17 +11,8 @@ export const DocView = memo((
 ) => {
   const { doc } = props
   return (
-    <DocViewContainer>
-      <CanvasToolbar>
-        <CanvasTitle>
-          {doc.getTitle()} - <em>大屏</em>
-        </CanvasTitle>
-        <Spring />
-        <Space className="actions">
-          <Button type="text" size="small" shape="circle" icon={<DeleteOutlined />} />
-        </Space>
-      </CanvasToolbar>
-      <CanvasContent className="canvas-content" />
-    </DocViewContainer>
+    <DocumentRoot doc={doc}>
+      <DocViewInner/>
+    </DocumentRoot>
   )
 })
