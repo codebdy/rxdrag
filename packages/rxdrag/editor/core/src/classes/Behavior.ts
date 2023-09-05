26f32200d770c7dbd09e3153b1cf721b5df9445f
Water.Li <lizongbin@hotmail.com>
2023-09-05 10:34:40 +0800

    backup

--- packages/rxdrag/editor/core/src/classes/Behavior.ts
+++ packages/rxdrag/editor/core/src/classes/Behavior.ts
@@ -18,12 +18,18 @@ export class Behavior implements IBehavior {
     if (isFn(this.rule.disabled)) {
       return this.rule.disabled(this.node, this.engine)
     }
+    if (this.rule.disabled !== undefined) {
+      return this.rule.disabled
+    }
     return false
   }
 
   selectable() {
     if (isFn(this.rule.selectable)) {
       return this.rule.selectable(this.node, this.engine)
+    }
+    if (this.rule.selectable !== undefined) {
+      return this.rule.selectable
     }
     return true
   }
@@ -32,12 +38,18 @@ export class Behavior implements IBehavior {
     if (isFn(this.rule.droppable)) {
       return this.rule.droppable(this.node, this.engine)
     }
+    if (this.rule.droppable !== undefined) {
+      return this.rule.droppable
+    }
     return false
   }
 
   draggable() {
-    if (isFn(this.rule.disabled)) {
-      return this.rule.disabled(this.node, this.engine)
+    if (isFn(this.rule.draggable)) {
+      return this.rule.draggable(this.node, this.engine)
+    }
+    if (this.rule.draggable !== undefined) {
+      return this.rule.draggable
     }
     return true
   }
@@ -46,12 +58,20 @@ export class Behavior implements IBehavior {
     if (isFn(this.rule.deletable)) {
       return this.rule.deletable(this.node, this.engine)
     }
+
+    if (this.rule.deletable !== undefined) {
+      return this.rule.deletable
+    }
     return true
   }
 
   cloneable() {
     if (isFn(this.rule.cloneable)) {
       return this.rule.cloneable(this.node, this.engine)
+    }
+
+    if (this.rule.cloneable !== undefined) {
+      return this.rule.cloneable
     }
     return true
   }
@@ -60,14 +80,14 @@ export class Behavior implements IBehavior {
     if (isFn(this.rule.resizable)) {
       return this.rule.resizable(this.node, this.engine)
     }
-    return undefined
+    return this.rule.resizable
   }
 
   moveable() {
     if (isFn(this.rule.moveable)) {
       return this.rule.moveable(this.node, this.engine)
     }
-    return undefined
+    return this.rule.moveable
   }
 
   equalRatio() {
@@ -81,12 +101,20 @@ export class Behavior implements IBehavior {
     if (isFn(this.rule.rotatable)) {
       return this.rule.rotatable(this.node, this.engine)
     }
+
+    if (this.rule.rotatable !== undefined) {
+      return this.rule.rotatable
+    }
     return false
   }
 
   allowChild(options?: CompareSource) {
     if (isFn(this.rule.allowChild)) {
       return this.rule.allowChild(options, this.engine)
+    }
+
+    if (this.rule.allowChild !== undefined) {
+      return this.rule.allowChild
     }
     return true
   };
@@ -95,12 +123,19 @@ export class Behavior implements IBehavior {
     if (isFn(this.rule.allowAppendTo)) {
       return this.rule.allowAppendTo(options, this.engine)
     }
+
+    if (this.rule.allowAppendTo !== undefined) {
+      return this.rule.allowAppendTo
+    }
     return true
   };
 
   allowSiblingsTo(options?: CompareSource) {
     if (isFn(this.rule.allowSiblingsTo)) {
       return this.rule.allowSiblingsTo(options, this.engine)
+    }
+    if (this.rule.allowSiblingsTo !== undefined) {
+      return this.rule.allowSiblingsTo
     }
     return true
   };
@@ -109,12 +144,18 @@ export class Behavior implements IBehavior {
     if (isFn(this.rule.noPlaceholder)) {
       return this.rule.noPlaceholder(this.node, this.engine)
     }
+    if (this.rule.noPlaceholder !== undefined) {
+      return this.rule.noPlaceholder
+    }
     return false
   }
 
   noRef() {
     if (isFn(this.rule.noRef)) {
       return this.rule.noRef(this.node, this.engine)
+    }
+    if (this.rule.noRef !== undefined) {
+      return this.rule.noRef
     }
     return false
   }
@@ -123,7 +164,9 @@ export class Behavior implements IBehavior {
     if (isFn(this.rule.lockable)) {
       return this.rule.lockable(this.node, this.engine)
     }
+    if (this.rule.lockable !== undefined) {
+      return this.rule.lockable
+    }
     return false
   }
-
 }
