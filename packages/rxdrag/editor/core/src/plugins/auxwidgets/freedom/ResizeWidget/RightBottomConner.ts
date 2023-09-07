6d1588be00cfa9d02aececf33db025b296b82305
Water.Li <lizongbin@hotmail.com>
2023-09-07 11:50:32 +0800

    backup

--- packages/rxdrag/editor/core/src/plugins/auxwidgets/freedom/ResizeWidget/RightBottomConner.ts
+++ packages/rxdrag/editor/core/src/plugins/auxwidgets/freedom/ResizeWidget/RightBottomConner.ts
@@ -1,5 +1,20 @@
 import { IDesignerEngine } from "../../../../interfaces";
 import { CornerHandler } from "./CornerHandler";
+import { HandlerSize } from "./consts";
+
+const svgIcon = `
+<svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M24.7292 11.4276L22.2351 11.4276L22.2351 16.8314C22.2351 16.8314 22.3331 20.0443 21.1916 21.1858C20.0501 22.3273 16.8313 22.2351 16.8313 22.2351L11.4276 22.2351L11.4276 24.7292L7.27084 21.4038L11.4276 18.0784L11.4276 20.5724L16 20.5724C16 20.5724 18.7753 20.7918 19.772 19.7951C20.7686 18.7985 20.5724 16 20.5724 16L20.5724 11.4276L18.0784 11.4276L21.4038 7.27084L24.7292 11.4276Z" fill="white"/>
+<path d="M24.7292 11.9276L25.7695 11.9276L25.1196 11.1152L21.7942 6.9585L21.4038 6.47045L21.0133 6.9585L17.6879 11.1152L17.0381 11.9276L18.0784 11.9276L20.0724 11.9276L20.0724 16L20.0724 16.0175L20.0736 16.0347L20.0736 16.0347L20.0736 16.0348L20.0736 16.035L20.0737 16.0355L20.074 16.04L20.0752 16.0604C20.0763 16.0789 20.0778 16.1072 20.0794 16.144C20.0826 16.2176 20.0862 16.3252 20.0875 16.4579C20.0902 16.7242 20.0838 17.0875 20.048 17.4782C20.012 17.871 19.9475 18.278 19.8389 18.6369C19.7283 19.0025 19.5847 19.2753 19.4184 19.4416C19.2524 19.6076 18.9815 19.7497 18.619 19.858C18.263 19.9643 17.8593 20.0261 17.4698 20.0593C17.0824 20.0923 16.7222 20.096 16.4583 20.0912C16.3267 20.0888 16.2201 20.0844 16.1472 20.0806C16.1108 20.0787 16.0828 20.0769 16.0645 20.0757L16.0443 20.0743L16.0399 20.074L16.0394 20.074L16.0392 20.074L16.0391 20.074L16.0391 20.074L16.0197 20.0724L16 20.0724L11.9276 20.0724L11.9276 18.0784L11.9276 17.0381L11.1152 17.6879L6.95849 21.0133L6.47045 21.4038L6.95849 21.7942L11.1152 25.1196L11.9276 25.7695L11.9276 24.7292L11.9276 22.7351L16.8251 22.7351L16.8292 22.7352L16.8595 22.7358C16.8854 22.7362 16.9228 22.7367 16.9703 22.7368C17.0652 22.7371 17.2007 22.7362 17.3665 22.7315C17.6972 22.722 18.152 22.697 18.6461 22.6346C19.1381 22.5724 19.6815 22.4717 20.1848 22.3063C20.6812 22.1432 21.1806 21.9038 21.5451 21.5393C21.9097 21.1747 22.1487 20.6756 22.3113 20.1796C22.4761 19.6767 22.5761 19.1339 22.6375 18.6425C22.6992 18.1491 22.7234 17.695 22.7324 17.3648C22.7369 17.1993 22.7376 17.0639 22.7371 16.9691C22.7368 16.9217 22.7363 16.8844 22.7358 16.8585L22.7352 16.8283L22.7351 16.8246L22.7351 11.9276L24.7292 11.9276Z" stroke="#363B3E"/>
+</svg>
+
+`
+const iconUrl = 'data:image/svg+xml;base64,'
+  + window.btoa(decodeURIComponent(svgIcon))
+
+const rotateCursor = `
+url('${iconUrl}') 12 12, pointer
+`
 
 export class RightBottomConner extends CornerHandler {
   constructor(container: HTMLDivElement, protected engine: IDesignerEngine) {
@@ -8,6 +23,26 @@ export class RightBottomConner extends CornerHandler {
     this.htmlElement.style.cursor = "nw-resize"
     this.htmlElement.style.right = "0"
     this.htmlElement.style.bottom = "0"
-    
+
+    let rightRotate = document.createElement('div')
+    rightRotate.style.position = "absolute"
+    rightRotate.style.cursor = rotateCursor
+    rightRotate.style.width = HandlerSize * 2 + 'px'
+    rightRotate.style.height = HandlerSize * 3 + 'px'
+    rightRotate.style.right = "-1px"
+    rightRotate.style.top = "0"
+    rightRotate.style.transform = "translate(100%, 0)"
+
+    this.hemlElementInner.appendChild(rightRotate)
+
+    rightRotate = document.createElement('div')
+    rightRotate.style.position = "absolute"
+    rightRotate.style.cursor = rotateCursor
+    rightRotate.style.width = HandlerSize * 3 + 'px'
+    rightRotate.style.height = HandlerSize * 2 + 'px'
+    rightRotate.style.left = "0"
+    rightRotate.style.bottom = "-1px"
+    rightRotate.style.transform = "translate(0, 100%)"
+    this.hemlElementInner.appendChild(rightRotate)
   }
 }
--- packages/rxdrag/editor/core/src/plugins/auxwidgets/freedom/ResizeWidget/RightTopConner.ts
+++ packages/rxdrag/editor/core/src/plugins/auxwidgets/freedom/ResizeWidget/RightTopConner.ts
@@ -2,7 +2,17 @@ import { IDesignerEngine } from "../../../../interfaces";
 import { CornerHandler } from "./CornerHandler";
 import { HandlerSize } from "./consts";
 
+const svgIcon = `
+<svg width="30" height="30" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M11.4276 7.27084V9.76488H16.8313C16.8313 9.76488 20.0443 9.66693 21.1857 10.8084C22.3272 11.9499 22.2351 15.1686 22.2351 15.1686V20.5724H24.7291L21.4037 24.7292L18.0783 20.5724H20.5724V16C20.5724 16 20.7917 13.2247 19.7951 12.228C18.7985 11.2314 16 11.4276 16 11.4276H11.4276V13.9216L7.27081 10.5962L11.4276 7.27084Z" fill="white"/>
+<path d="M11.9276 7.27084V6.23052L11.1152 6.8804L6.95847 10.2058L6.47042 10.5962L6.95847 10.9867L11.1152 14.3121L11.9276 14.9619V13.9216V11.9276H16H16.0175L16.0347 11.9264L16.0347 11.9264L16.0348 11.9264L16.0349 11.9264L16.0355 11.9263L16.04 11.926L16.0604 11.9248C16.0789 11.9237 16.1071 11.9222 16.1439 11.9206C16.2175 11.9174 16.3251 11.9138 16.4579 11.9125C16.7242 11.9098 17.0875 11.9162 17.4782 11.952C17.8709 11.988 18.278 12.0525 18.6369 12.1611C19.0025 12.2717 19.2752 12.4153 19.4415 12.5816C19.6075 12.7476 19.7496 13.0185 19.8579 13.381C19.9643 13.737 20.0261 14.1407 20.0593 14.5302C20.0923 14.9176 20.0959 15.2777 20.0912 15.5417C20.0888 15.6733 20.0843 15.7799 20.0805 15.8528C20.0786 15.8892 20.0769 15.9172 20.0757 15.9355L20.0743 15.9557L20.074 15.9601L20.0739 15.9606L20.0739 15.9608L20.0739 15.9609L20.0739 15.9609L20.0724 15.9803V16V20.0724H18.0783H17.038L17.6879 20.8848L21.0133 25.0415L21.4037 25.5295L21.7942 25.0415L25.1196 20.8848L25.7694 20.0724H24.7291H22.7351V15.1749L22.7352 15.1708L22.7358 15.1405C22.7362 15.1146 22.7367 15.0772 22.7368 15.0297C22.7371 14.9348 22.7362 14.7993 22.7314 14.6335C22.7219 14.3028 22.6969 13.848 22.6345 13.3539C22.5724 12.8619 22.4716 12.3184 22.3063 11.8152C22.1432 11.3188 21.9038 10.8194 21.5393 10.4549C21.1747 10.0903 20.6756 9.85126 20.1795 9.68867C19.6767 9.52386 19.1339 9.42385 18.6425 9.36245C18.1491 9.3008 17.6949 9.27655 17.3647 9.2676C17.1992 9.26311 17.0639 9.26244 16.9691 9.26291C16.9217 9.26314 16.8844 9.26367 16.8585 9.26415L16.8283 9.26479L16.8246 9.26488H11.9276V7.27084Z" stroke="#363B3E"/>
+</svg>
+`
+const iconUrl ='data:image/svg+xml;base64,' 
+				+ window.btoa(decodeURIComponent(svgIcon))
+
 const rotateCursor = `
+url('${iconUrl}') 12 12, pointer
 `
 
 export class RightTopConner extends CornerHandler {
@@ -15,8 +25,7 @@ export class RightTopConner extends CornerHandler {
 
     let rightRotate = document.createElement('div')
     rightRotate.style.position = "absolute"
-    rightRotate.style.cursor = "pointer"
-    rightRotate.style.backgroundColor = "red"
+    rightRotate.style.cursor = rotateCursor
     rightRotate.style.width = HandlerSize * 2 + 'px'
     rightRotate.style.height = HandlerSize * 3 + 'px'
     rightRotate.style.right = "-1px"
@@ -27,9 +36,8 @@ export class RightTopConner extends CornerHandler {
 
     rightRotate = document.createElement('div')
     rightRotate.style.position = "absolute"
-    rightRotate.style.cursor = "pointer"
-    rightRotate.style.backgroundColor = "red"
-    rightRotate.style.width = HandlerSize * 3 + 'px'
+    rightRotate.style.cursor = rotateCursor
+     rightRotate.style.width = HandlerSize * 3 + 'px'
     rightRotate.style.height = HandlerSize * 2 + 'px'
     rightRotate.style.left = "0"
     rightRotate.style.top = "-1px"
