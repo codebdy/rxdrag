e29d1f8ac9f05d0947b49053082bf3d1775f50a6
Water.Li <lizongbin@hotmail.com>
2023-09-08 12:34:26 +0800

    backup

---
+++ examples/app-designer-example/src/data/app.ts
@@ -0,0 +1,6 @@
+import { IApp } from "../interfaces";
+
+export const app: IApp = {
+  id: "app1",
+  title: "火星改造项目"
+}
--- examples/app-designer-example/src/hooks/useQueryApp.ts
+++ examples/app-designer-example/src/hooks/useQueryApp.ts
@@ -1,14 +1,13 @@
+import { app } from "../data";
 import { IApp } from "../interfaces";
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function useQueryApp(id?: string): {
   app?: IApp,
   loading?: boolean,
 } {
   return {
-    app: {
-      id: "app1",
-      title: "火星改造项目"
-    },
+    app:app,
     loading: false,
   }
 }
