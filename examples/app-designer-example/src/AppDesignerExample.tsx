ed243165761db1cd42bbe163c21a6fa9c54d949a
Water.Li <lizongbin@hotmail.com>
2023-09-08 21:54:27 +0800

    backup

--- examples/app-designer-example/src/AppDesignerExample.tsx
+++ examples/app-designer-example/src/AppDesignerExample.tsx
@@ -1,5 +1,5 @@
 import { ConfigProvider, theme } from "antd"
-import { memo, useState } from "react"
+import { memo, useEffect, useMemo, useState } from "react"
 import { Toolbar } from "./Toolbar"
 import { UiDesigner } from "./UiDesigner"
 import { AppContext } from "./contexts"
@@ -7,6 +7,9 @@ import { useQueryApp } from "./hooks/useQueryApp"
 import { ThemeRoot } from "./ThemeRoot"
 import { DeviceType } from "./interfaces"
 import styled from "styled-components"
+import { LocalesContext } from "@rxdrag/react-locales"
+import { LocalesManager } from "@rxdrag/locales"
+import { appDesignerLocales } from "./locales"
 
 const Container = styled.div`
   width: 100%;
@@ -27,30 +30,38 @@ export const AppDesignerExample = memo((props: {
   const { app } = useQueryApp("app1")
   const [device, setDevice] = useState<DeviceType>(DeviceType.admin)
 
+  const localesManger = useMemo(() => new LocalesManager(), [])
+
+  useEffect(() => {
+    localesManger.registerLocales(appDesignerLocales)
+  }, [localesManger])
+
   return (
     <ConfigProvider
       theme={{
         algorithm: themeMode === "dark" ? theme.darkAlgorithm : theme.defaultAlgorithm
       }}
     >
-      <ThemeRoot mode={themeMode}>
-        <AppContext.Provider value={app}>
-          <Container className="zoomable-editor">
-            <Toolbar
-              device={device}
-              onDeviceChange={setDevice}
-              themeMode={themeMode}
-              onThemeModeChange={setThemeMode}
-            />
-            <UiDesigner
-              device={device}
-              canvasUrl={canvasUrl}
-              previewUrl={previewUrl}
-              themeMode={themeMode}
-            />
-          </Container>
-        </AppContext.Provider>
-      </ThemeRoot>
+      <LocalesContext.Provider value={localesManger}>
+        <ThemeRoot mode={themeMode}>
+          <AppContext.Provider value={app}>
+            <Container className="zoomable-editor">
+              <Toolbar
+                device={device}
+                onDeviceChange={setDevice}
+                themeMode={themeMode}
+                onThemeModeChange={setThemeMode}
+              />
+              <UiDesigner
+                device={device}
+                canvasUrl={canvasUrl}
+                previewUrl={previewUrl}
+                themeMode={themeMode}
+              />
+            </Container>
+          </AppContext.Provider>
+        </ThemeRoot>
+      </LocalesContext.Provider>
     </ConfigProvider>
   )
 })
--- examples/app-designer-example/src/UiDesigner/ModuleUiDesigner/index.tsx
+++ examples/app-designer-example/src/UiDesigner/ModuleUiDesigner/index.tsx
@@ -2,10 +2,7 @@ import { EditorScope } from "@rxdrag/react-antd-shell"
 import { memo } from "react"
 import { ModuleUiDesignerInner } from "./ModuleUiDesignerInner"
 import { setterLocales, minionsMaterialCategories, minionsLocales, controllerDefines, materials } from "example-common"
-import _ from "lodash"
 import { DeviceType, ThemeMode } from "../../interfaces"
-import { appDesignerLocales } from "../../locales"
-
 
 export const ModuleUiDesigner = memo((
   props: {
@@ -18,7 +15,7 @@ export const ModuleUiDesigner = memo((
   const { device, canvasUrl, previewUrl, themeMode } = props;
   return (
     <EditorScope
-      locales={{ ..._.merge(setterLocales, appDesignerLocales) }}
+      locales={setterLocales}
       canvasUrl={canvasUrl}
       previewUrl={previewUrl}
       themeMode={themeMode}
