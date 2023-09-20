37706ede7ba5cc68534c6e3d402e9c103a5aaf21
Water.Li <lizongbin@hotmail.com>
2023-09-20 17:21:26 +0800

    Update Toolbox.tsx

--- examples/app-designer-example/src/FrontendDesigner/MenuDesigner/Toolbox.tsx
+++ examples/app-designer-example/src/FrontendDesigner/MenuDesigner/Toolbox.tsx
@@ -1,18 +1,35 @@
 import { useTranslate } from "@rxdrag/react-locales"
 import { ResourceItem, defaultMenuResources } from "@rxdrag/react-menu-designer"
-import { Collapse, CollapseProps } from "antd"
+import { Collapse, CollapseProps, Tree } from "antd"
 import { memo, useMemo } from "react"
 import styled from "styled-components"
+import { useAppFrontend } from "../../hooks/useAppFrontend"
+import { DataNode } from "antd/es/tree"
+
+const { DirectoryTree } = Tree;
 
 const ItemContainer = styled.div`
   position: relative;
-  height: 48px;
+  height: 40px;
   box-sizing: border-box;
   width: 100%;
   margin: 8px 0;
   &.dragging{
     opacity: 0.6;
   }
+`
+
+const ModuleItemCOntainer = styled.div`
+  position: relative;
+  height: 40px;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  &.dragging{
+    opacity: 0.6;
+  }
+
+  cursor: move;
 `
 
 const StyledCollapse = styled(Collapse)`
@@ -29,14 +46,54 @@ const StyledCollapse = styled(Collapse)`
   .ant-collapse-content{
     background-color: transparent;
   }
+  .ant-tree{
+    background-color: transparent;
+    .ant-tree-treenode{
+      display: flex;
+      .ant-tree-node-content-wrapper{
+        display: flex;
+        align-items: center;
+      }
+      .ant-tree-indent-unit{
+        width: 16px;
+      }
+      .ant-tree-title{
+        flex:1;
+      }
+      .ant-tree-switcher{
+        display: flex;
+        align-items: center;
+        width: 12px;
+      }
+    }
+  }
 `
 
 export const Toolbox = memo(() => {
   const t = useTranslate()
+  const appFront = useAppFrontend()
+
+  const treeData: DataNode[] = useMemo(() => {
+    return appFront?.moduleCategories?.map(category => {
+      return {
+        key: category.id,
+        title: category.title,
+        children: category.modules?.map(module => {
+          return {
+            key: module.id,
+            title: <ModuleItemCOntainer>
+              {module.title}
+            </ModuleItemCOntainer>,
+            isLeaf: true,
+          }
+        })
+      }
+    }) || []
+  }, [appFront?.moduleCategories])
 
   const items: CollapseProps['items'] = useMemo(() => [
     {
-      key: '1',
+      key: 'base',
       label: t("base"),
       children: defaultMenuResources.map((resrouce) => {
         return (<ItemContainer key={resrouce.id}>
@@ -45,10 +102,16 @@ export const Toolbox = memo(() => {
       }),
     },
     {
-      key: '2',
+      key: 'modules',
       label: t("modules"),
+      children: <DirectoryTree
+        selectable={false}
+        multiple={false}
+        defaultExpandAll
+        treeData={treeData}
+      />
     },
-  ], [t]);
+  ], [t, treeData]);
 
 
   return (
