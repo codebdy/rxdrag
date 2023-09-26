02b852ea0a6f43420e89f421f5986e138244bf6e
Water.Li <lizongbin@hotmail.com>
2023-09-26 10:32:37 +0800

    backup

--- examples/example-common/src/materials/page/schema.ts
+++ examples/example-common/src/materials/page/schema.ts
@@ -1,4 +1,23 @@
-import { createSchema } from "@rxdrag/react-antd-materials";
+import { SchemaOptions, createSchema } from "@rxdrag/react-antd-materials";
 import { INodeSchema } from "@rxdrag/schema";
 
-export const paeSchema: INodeSchema = createSchema()
+const scehmaOptions: SchemaOptions = {
+  canBindField: true,
+  propSchemas: [
+    {
+      componentName: "Input",
+      "x-field": {
+        label: "$title",
+        name: "title",
+      }
+    },
+  ],
+  slotSchemas: [
+    {
+      name: "icon",
+      label: "$icon"
+    }
+  ],
+}
+
+export const schema: INodeSchema = createSchema(scehmaOptions)
