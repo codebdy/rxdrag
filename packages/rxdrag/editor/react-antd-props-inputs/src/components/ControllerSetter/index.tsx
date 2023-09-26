6a5243dc2292805136e72a122f794a63c9304c86
Water.Li <lizongbin@hotmail.com>
2023-09-26 11:32:00 +0800

    backup

--- packages/rxdrag/editor/react-antd-props-inputs/src/components/ControllerSetter/index.tsx
+++ packages/rxdrag/editor/react-antd-props-inputs/src/components/ControllerSetter/index.tsx
@@ -1,22 +1,50 @@
+import { IControllerMeta } from "@rxdrag/minions-runtime-react"
 import { useSettersTranslate } from "@rxdrag/react-core"
+import { createId } from "@rxdrag/shared"
 import { Form, Input, Switch } from "antd"
-import { memo } from "react"
+import { memo, useCallback } from "react"
 
-export const ControllerSetter = memo(() => {
+export const ControllerSetter = memo((
+  props: {
+    value?: IControllerMeta | null,
+    onChange?: (value?: IControllerMeta | null) => void
+  }
+) => {
+  const { value, onChange } = props;
   const t = useSettersTranslate()
+
+  const handleSwitch = useCallback((checked: boolean) => {
+    if (checked) {
+      onChange?.({ ...value, id: createId() })
+    } else {
+      onChange?.(null)
+    }
+  }, [onChange, value])
+
+  const handleNameChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    if (value) {
+      onChange?.({ ...value, name: e.target.value })
+    }
+  }, [onChange, value])
 
   return (
     <>
       <Form.Item
         label={t("joinFlow")}
       >
-        <Switch />
+        <Switch
+          checked={!!value?.id}
+          onChange={handleSwitch}
+        />
       </Form.Item>
-      <Form.Item
-        label={t("name")}
-      >
-        <Input />
-      </Form.Item>
+      {
+        value?.id &&
+        <Form.Item
+          label={t("name")}
+        >
+          <Input onChange={handleNameChange} />
+        </Form.Item>
+      }
     </>
   )
 })
