36e422eaece28b93a428e4ca1a6307385584e3c7
Water.Li <lizongbin@hotmail.com>
2023-09-29 09:33:53 +0800

    backup

--- examples/app-designer-example/src/FrontendDesigner/BottomConsole/FlowDesigner/index.tsx
+++ examples/app-designer-example/src/FrontendDesigner/BottomConsole/FlowDesigner/index.tsx
@@ -28,6 +28,7 @@ enum NavType {
   toolbox = "toolbox",
   flows = "flows",
   fxes = "fxes",
+  model = "model",
 }
 
 export const FlowDesigner = memo(() => {
@@ -45,6 +46,11 @@ export const FlowDesigner = memo(() => {
   const handleToggleComponents = useCallback(() => {
     setNavType(type => type === NavType.componentTree ? null : NavType.componentTree)
   }, [])
+
+  const handleToggleModel = useCallback(() => {
+    setNavType(type => type === NavType.model ? null : NavType.model)
+  }, [])
+
 
   const handleToggleToolbox = useCallback(() => {
     setNavType(type => type === NavType.toolbox ? null : NavType.toolbox)
@@ -93,6 +99,13 @@ export const FlowDesigner = memo(() => {
               type={navType === NavType.componentTree ? "primary" : "text"}
               icon={<PartitionOutlined />}
               onClick={handleToggleComponents}
+            />
+          </Tooltip>
+          <Tooltip title="数据模型" placement="right">
+            <NavButton
+              type={navType === NavType.model ? "primary" : "text"}
+              icon={<svg viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="1em" height="1em"><path d="M138.715429 781.568l336.420571 191.140571c24.868571 14.153143 48 14.573714 73.728 0l336.420571-191.140571c39.862857-22.710857 61.714286-45.860571 61.714286-107.995429V334.134857c0-47.561143-18.852571-74.569143-53.997714-94.72l-302.994286-172.269714c-52.717714-30.427429-104.155429-29.988571-156.013714 0l-302.994286 172.288c-35.145143 20.132571-53.997714 47.140571-53.997714 94.72v339.419428c0 62.134857 21.851429 85.284571 61.714286 107.995429zM512 476.013714L185.417143 291.291429l279.003428-159.451429c32.585143-18.413714 61.714286-18.834286 95.158858 0l278.985142 159.451429zM179.017143 721.554286c-24.448-13.714286-32.585143-27.008-32.585143-50.139429V351.707429l329.563429 188.16v350.555428z m665.984 0l-296.996572 168.850285V539.849143l329.563429-188.141714V671.451429c0 23.131429-8.137143 36.425143-32.566857 50.139428z"></path></svg>}
+              onClick={handleToggleModel}
             />
           </Tooltip>
           <Tooltip title="行为流" placement="right">
