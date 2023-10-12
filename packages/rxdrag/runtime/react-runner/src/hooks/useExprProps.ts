01d81fae75b43f3663a471d7af8dac67bc48465d
Water.Li <lizongbin@hotmail.com>
2023-10-12 18:22:33 +0800

    backup

--- packages/rxdrag/runtime/react-runner/src/hooks/useExprProps.ts
+++ packages/rxdrag/runtime/react-runner/src/hooks/useExprProps.ts
@@ -1,18 +1,21 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useForm } from "@rxdrag/react-fieldy";
 import { useCallback, useEffect, useState } from "react";
+import { useCalculateExpr } from "./useCalculateExpr";
 
 export function useExprProps(exprs: Record<string, string | null>) {
   const [changedProps, setChangedProps] = useState<Record<string, unknown>>({})
   const form = useForm()
+  const calculate = useCalculateExpr(form)
 
   const handleFormValueChange = useCallback(() => {
     const props: Record<string, unknown> = {}
-    for(const key of Object.keys(exprs) ){
+    for (const key of Object.keys(exprs)) {
       const expr = exprs[key]
+      props[key] = calculate(expr)
     }
-  setChangedProps(props)
-  }, [exprs])
+    setChangedProps(props)
+  }, [calculate, exprs])
 
   useEffect(() => {
     const unsub = form?.onValueChange(handleFormValueChange)
