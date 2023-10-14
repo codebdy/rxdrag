9fd67332141251e2c29cd6afa0434c611f104282
Water.Li <lizongbin@hotmail.com>
2023-10-14 18:20:41 +0800

    backup

--- packages/rxdrag/runtime/react-runner/src/ComponentField.tsx
+++ packages/rxdrag/runtime/react-runner/src/ComponentField.tsx
@@ -1,6 +1,5 @@
 import React, { memo, useMemo } from "react"
-import { parsePathValue } from "@rxdrag/fieldy"
-import { XField, useFieldNode } from "@rxdrag/react-fieldy"
+import { XField } from "@rxdrag/react-fieldy"
 import { IFieldMeta } from "@rxdrag/fieldy"
 
 export const ComponentField = memo((
@@ -10,23 +9,23 @@ export const ComponentField = memo((
   }
 ) => {
   const { fieldMeta, children } = props
-  const parentField = useFieldNode()
+  //const parentField = useFieldNode()
 
   const view = useMemo(() => {
     if (fieldMeta?.name) {
       //自动渲染没有数组，所以可以这么处理
-      const initialValue = parsePathValue(parentField?.getInitialValue(), fieldMeta.name.toString())
-      const defaultValue = parsePathValue(parentField?.getDefaultValue(), fieldMeta.name.toString())
+      //const initialValue = parsePathValue(parentField?.getInitialValue(), fieldMeta.name.toString())
+      //const defaultValue = parsePathValue(parentField?.getDefaultValue(), fieldMeta.name.toString())
       return <XField
         fieldMeta={fieldMeta}
-        initialValue={initialValue}
-        defaultValue={fieldMeta.defaultValue !== undefined ? fieldMeta.defaultValue : defaultValue}
+      //initialValue={initialValue}
+      //defaultValue={fieldMeta.defaultValue !== undefined ? fieldMeta.defaultValue : defaultValue}
       >
         {children}
       </XField>
     }
     return <>{children}</>
-  }, [children, fieldMeta, parentField])
+  }, [children, fieldMeta])
 
   return view
 })
