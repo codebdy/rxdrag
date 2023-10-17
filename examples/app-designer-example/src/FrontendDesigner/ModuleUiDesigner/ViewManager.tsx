8a5d7e49a7a1d72e427f8557b6947442138bbb08
Water.Li <lizongbin@hotmail.com>
2023-10-17 12:23:46 +0800

    Update ViewManager.tsx

--- examples/app-designer-example/src/FrontendDesigner/ModuleUiDesigner/ViewManager.tsx
+++ examples/app-designer-example/src/FrontendDesigner/ModuleUiDesigner/ViewManager.tsx
@@ -1,20 +1,56 @@
 import { NumberOutlined } from "@ant-design/icons"
-import { Tooltip, Button } from "antd"
-import { memo } from "react"
+import { Button, Popover, Space } from "antd"
+import { memo, useCallback, useState } from "react"
+import styled from "styled-components"
+
+const Container = styled.div`
+  display: flex;
+  flex-flow: column;
+`
+
+const Content = styled.div`
+  
+`
+
+const Footer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`
 
 export const ViewManager = memo(() => {
+  const [open, setOpen] = useState<boolean>()
+
+  const handleClose = useCallback(() => {
+    setOpen(false)
+  }, [])
+
   return (
-    <Tooltip title={"视图管理"} placement="topRight">
-      <div>
-        <Button
-          type={"text"}
-          size="small"
-          icon={
-            <NumberOutlined />
-          }
-        //onClick={handlePreviewClick}
-        />
-      </div>
-    </Tooltip>
+    <Popover
+      open={open}
+      placement="topRight"
+      title={"视图管理"}
+      content={<Container>
+        <Content>
+
+        </Content>
+        <Footer>
+          <Space>
+            <Button onClick={handleClose}>取消</Button>
+            <Button type="primary">确定</Button>
+          </Space>
+        </Footer>
+      </Container>}
+      trigger="click"
+      onOpenChange={setOpen}
+    >
+      <Button
+        type={open ? "link" : "text"}
+        size="small"
+        icon={
+          <NumberOutlined />
+        }
+      />
+    </Popover>
   )
 })
