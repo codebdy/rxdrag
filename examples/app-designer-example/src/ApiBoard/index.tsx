2ae13b5e85fe8a94e0ae7590ae53cc1d791976ff
Water.Li <lizongbin@hotmail.com>
2023-10-18 18:22:23 +0800

    制作apiboard

--- examples/app-designer-example/src/ApiBoard/index.tsx
+++ examples/app-designer-example/src/ApiBoard/index.tsx
@@ -1,7 +1,37 @@
-import { memo } from "react"
+import GraphiQL from "graphiql";
+import "graphiql/graphiql.css";
+import { memo, useMemo } from "react";
+import React from "react";
+import "./index.less";
+import { createGraphiQLFetcher } from "@graphiql/toolkit";
+import "./graphiql-dark-mode.css"
 
+//例子連接
+//https://github.com/graphql/graphiql/blob/main/packages/graphiql-toolkit/docs/create-fetcher.md#subscriptionurl
 export const ApiBoard = memo(() => {
-  return (<>
-    ApiBoard
-  </>)
-})
+  // const realAppId = useEdittingAppId();
+  // const token = useToken();
+  // const endppoint = useEndpoint();
+  const fetcher = useMemo(() => {
+    const fetcher = createGraphiQLFetcher({
+      url: "",
+      // legacyWsClient: new SubscriptionClient(SERVER_SUBSCRIPTION_URL),
+      // headers: {
+      //   [HEADER_AUTHORIZATION]: token ? `${TOKEN_PREFIX}${token}` : "",
+      //   [HEADER_LEDA_APPID]: realAppId,
+      // }
+    });
+
+    return fetcher;
+  }, []);
+
+  return (
+    <div className="api-board">
+      <GraphiQL
+        headerEditorEnabled
+      fetcher={fetcher}
+      // query=""
+      />
+    </div>
+  );
+});
