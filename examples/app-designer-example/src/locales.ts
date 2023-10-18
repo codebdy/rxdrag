92d05d07f8edfae66de29e29db91eb9288af34bd
Water.Li <lizongbin@hotmail.com>
2023-10-18 14:02:47 +0800

    backup

--- examples/app-designer-example/src/locales.ts
+++ examples/app-designer-example/src/locales.ts
@@ -4,7 +4,7 @@ export const appDesignerLocales: ILocales = {
   "zh-CN": {
     appDesigner: {
       website: "门户",
-      h5: "H5",
+      h5: "移动端",
       admin: "管理端",
       largeScreen: "大屏"
     }
@@ -13,7 +13,7 @@ export const appDesignerLocales: ILocales = {
   'en-US': {
     appDesigner: {
       website: "Website",
-      h5: "H5",
+      h5: "Mobile H5",
       admin: "Admin",
       largeScreen: "Large screen"
     }
