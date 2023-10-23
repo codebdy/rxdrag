631a98a276f698b95a9547546af712dab5e3a1c7
Water.Li <lizongbin@hotmail.com>
2023-10-23 22:59:29 +0800

    Update ExpressionInput.tsx

--- examples/app-designer-example/src/minions/materials/setters/QueryParamsInput/ExpressionInput/ExpressionInput.tsx
+++ examples/app-designer-example/src/minions/materials/setters/QueryParamsInput/ExpressionInput/ExpressionInput.tsx
@@ -13,12 +13,12 @@ export const ExpressionInput = memo((
 ) => {
   const { entityId } = props;
   const [value, setValue] = useState<string>();
-  const entity = useEnitity(entityId)
+  const rootEntity = useEnitity(entityId)
   const getEntity = useGetEntity()
   const t = useTranslate()
   const [treeData, setTreeData] = useState<Omit<DefaultOptionType, 'label'>[]>([]);
 
-  const getEntityNodes = useCallback((entity?: EntityMeta, pId?: string) => {
+  const getEntityNodes = useCallback((entity?: EntityMeta, pId?: string, parentTypeIds?: string[]) => {
     return [
       ...entity?.attributes?.map((attr) => {
         const id = pId ? (pId + "." + attr.name) : attr.name
@@ -30,30 +30,39 @@ export const ExpressionInput = memo((
           isLeaf: true
         }
       }) || [],
-      ...entity?.associations?.map((asso) => {
+      ...entity?.associations?.filter((asso) => {
+        if (parentTypeIds?.find(tid => tid === asso.typeId)) {
+          return false
+        }
+        return true
+      }).map((asso) => {
         const assoEntity = getEntity(asso.typeId)
         const id = pId ? (pId + "." + asso.name) : asso.name
         return {
           id: id,
           pId,
           value: id,
-          title: asso.label || asso.name || asso.id + ":" + assoEntity?.label || assoEntity?.name,
+          title: (asso.label || asso.name || asso.id) + " : " + (assoEntity?.label || assoEntity?.name),
           selectable: false,
           typeId: asso.typeId,
+          parentTypeIds: [...parentTypeIds || [], asso.typeId]
         }
       }) || [],
     ]
   }, [getEntity])
 
   useEffect(() => {
-    setTreeData(getEntityNodes(entity))
-  }, [entity, getEntityNodes])
+    if (rootEntity) {
+      setTreeData(getEntityNodes(rootEntity, undefined, [rootEntity.uuid]))
+    }
+
+  }, [rootEntity, getEntityNodes])
 
   const onLoadData: TreeSelectProps['loadData'] = (arg) =>
     new Promise((resolve) => {
       const typeEntity = getEntity(arg.typeId)
       setTreeData(
-        treeData.concat(getEntityNodes(typeEntity, arg.id))
+        treeData.concat(getEntityNodes(typeEntity, arg.id, arg.parentTypeIds))
       );
       resolve(undefined);
     });
